2ea874b65d435ba1a5ae6d20949b6fd14f8b99fe
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-01-12 14:50:05 +0000

    WIP: creating run logger outline

--- src/main.py
+++ src/main.py
@@ -9,3 +9,23 @@ def run_pipeline():
 
 def add_numbers(a, b):
     return a + B
+
+from utils import runlog
+import pandas as pd
+import numpy as np 
+
+
+def dummy_function(seed=42):
+    np.random.seed(seed=seed)
+    df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+    return df
+
+def manipulate_df(df):
+    df = df * 2
+    return df
+
+if __name__ == "__main__":
+    # Do something
+    df = dummy_function()
+    df = manipulate_df(df)
+    print(df)
