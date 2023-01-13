797524fd5ba595d29d54827f955bd60a22fc69f6
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-01-13 11:38:44 +0000

    Added and edited config/logging files

--- src/main.py
+++ src/main.py
@@ -1,6 +1,38 @@
-from utils import logger_mod
+from src.utils.configuration import ConfigReader
+import logging
+import time
+from datetime import datetime, timedelta
+import sys
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+# set up different logs for different modules. (i.e. data cleaning, data visualisation etc.)
+file_handler = logging.FileHandler('src/utils/mylog.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+
+logger.addHandler(file_handler)
+
+#%%
 
 def add_numbers(a, b):
-    return a + B
+    logger.info("Starting function call")
+
+    total_time = round(time.time(), 0)
+    timestamp = str(datetime.now())
+    config = ConfigReader('src/utils/testconfig.ini')
+    global_config = config.config_setup("global")
+    
+    try:
+        c = a + b
+        logger.info("Finished function call")
+        return c
+    except TypeError:
+        print("error")
+        logger.error("Error with input.")
+        return
+        
+    
