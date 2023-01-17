678ba13c661257acba26408a402cc5eb36dde991
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-01-17 10:21:13 +0000

    Changed log output format

--- src/main.py
+++ src/main.py
@@ -64,17 +64,23 @@ if __name__ == "__main__":
         
 #%%
 import logging
-from src.utils.crime_logger_mod import logger_creator, time_logger_wrap
+import src.utils.crime_logger_mod
+#from src.utils.crime_logger_mod import logger_creator, time_logger_wrap, exception_wrap
 import configparser
 config= configparser.ConfigParser()
 config.read(r'src/utils/testconfig.ini')
 global_config = config['global']
 
 logger_creator(global_config)
+
 LOGGER = logging.getLogger(__name__)
+if logger.hasHandlers():
+    logger.handlers = []
+
 @time_logger_wrap
+@exception_wrap
 def add(a,b):
     c = a + b    
     return c
 
-add(1,2)
+add(1,6)
