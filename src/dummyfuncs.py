91c905ab98c2ee2b1beb0f279e4dd4ffc1467be6
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-01-18 09:59:30 +0000

    Ran pre-commit on all files with auto-fixes

--- src/dummyfuncs.py
+++ src/dummyfuncs.py
@@ -1,21 +1,24 @@
 import logging
 from src.utils.loggingfuncs import logger_creator, time_logger_wrap, exception_wrap
 import configparser
-config= configparser.ConfigParser()
-config.read(r'src/utils/testconfig.ini')
-global_config = config['global']
+
+config = configparser.ConfigParser()
+config.read(r"src/utils/testconfig.ini")
+global_config = config["global"]
 
 logger_creator(global_config)
 
 logger = logging.getLogger(__name__)
 
-#if logger.hasHandlers():
-    #logger.handlers = []
+# if logger.hasHandlers():
+# logger.handlers = []
+
 
 @time_logger_wrap
 @exception_wrap
-def add(a,b):
-    c = a + b 
+def add(a, b):
+    c = a + b
     return c
 
-add(1,2)
+
+add(1, 2)
