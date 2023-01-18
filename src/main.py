4d25694dd6234ecf3c7c766e87c10f3bc1e5b1aa
jwestw <james.westwood@ons.gov.uk>
2023-01-18 10:13:13 +0000

    Attempt at run logger. WIP

--- src/main.py
+++ src/main.py
@@ -4,15 +4,38 @@
 from utils import runlog
 import pandas as pd
 import numpy as np 
+import logging
+from _version import __version__ as version
+
+
+config = {
+    "run_id": "test",
+    "version": version,
+    "log_path": "data/log",
+    "log_file": "runlog.hdf5",
+    "log_table": "runlog",
+    "log_db": "test",
+    "log_format": "csv",
+    "log_mode": "overwrite",
+}
+
+RUN_LOGGER = runlog.RunLog(config, version)
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+LOGGER.info("Starting pipeline")
 
 
 def dummy_function(seed=42):
     np.random.seed(seed=seed)
-    df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+    df = pd.DataFrame(np.random.randint(0, 100,size=(100, 4)), columns=list('ABCD'))
+    dummy_log = LOGGER.info("Dummy dataframe has been created")
     return df
 
 def manipulate_df(df):
     df = df * 2
+    LOGGER.info("Dummy dataframe has been manipulated")
     return df
 
 if __name__ == "__main__":
