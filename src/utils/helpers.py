27167e42c326bd0977b517294c4cee40a48bdd07
jwestw <james.westwood@ons.gov.uk>
2023-01-24 11:23:09 +0000

    Create config parser

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,17 @@
 """Define helper functions that wrap regularly-used functions."""
 
-import configparser
+import yaml
 
-CONFIG_FILE = "config.yaml"
+
+class Config_settings:
+    """Get the config settings from the config file."""
+    def __init__(self):
+        self.config_file = "src/config.yaml"
+        self.config_dict = self.get_config_settings()
+
+    def get_config_settings(self):
+        """Get the config settings from the config file."""
+        with open(self.config_file, "r") as file:
+            config = yaml.safe_load(file)
+        return config
 
