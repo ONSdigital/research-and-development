1f49193b9accb5ee74786d01cf9f513d7d72daac
jwestw <james.westwood@ons.gov.uk>
2023-01-24 11:22:58 +0000

    Insert details of run into runlogger

--- src/main.py
+++ src/main.py
@@ -6,26 +6,18 @@ import pandas as pd
 import numpy as np 
 import logging
 from _version import __version__ as version
-
-
-config = {
-    "run_id": "test",
-    "version": version,
-    "log_path": "data/log",
-    "log_file": "runlog.hdf5",
-    "log_table": "runlog",
-    "log_db": "test",
-    "log_format": "csv",
-    "log_mode": "overwrite",
-}
-
-RUN_LOGGER = runlog.RunLog(config, version)
+from utils.helpers import Config_settings
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
+# create a file handler
+handler = logging.FileHandler('logs/pipeline.log')
 
 LOGGER.info("Starting pipeline")
 
+# Get the config settings using the Config_settings class
+conf_obj = Config_settings()
+config = conf_obj.get_config_settings()
 
 def dummy_function(seed=42):
     np.random.seed(seed=seed)
@@ -38,8 +30,22 @@ def manipulate_df(df):
     LOGGER.info("Dummy dataframe has been manipulated")
     return df
 
+# create a runlog instance
+runlog_obj = runlog.RunLog(config, version)
+
+# insert the config settings into the runlog
+runlog_obj.get_config_settings(config)
+
 if __name__ == "__main__":
+    
     # Do something
     df = dummy_function()
     df = manipulate_df(df)
     print(df)
+
+
+# insert the time taken into the runlog
+fake_time_taken = 10
+runlog_obj.record_time_taken(time_taken=fake_time_taken)
+
+# insert the logs into the runlog
