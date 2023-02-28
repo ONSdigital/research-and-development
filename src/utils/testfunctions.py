b104ba983edee590a0f3ee246dc7b50dfee994e9
jwestw <james.westwood@ons.gov.uk>
2023-02-28 10:58:57 +0000

    adding an raise error and type hints

--- src/utils/testfunctions.py
+++ src/utils/testfunctions.py
@@ -1,7 +1,10 @@
 import time
 
-def add(a, b):
+def add(a:int, b: int):
     """Testing multiple wrappers for one function"""
+    # Raise error if a or b is not an integer
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError("a and b must be integers")
     c = a + b
     time.sleep(0.5)
     return c
