3786abb756257dd3fa2ebe5d1694333cd3fd3ce6
jwestw <james.westwood@ons.gov.uk>
2023-02-28 10:59:22 +0000

    test add fails when the arguments are not ints

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,18 @@
 """Create a test suite for the main module."""
 
-# test the add function from testfunctions.py which is imported into main.py
+import pytest
+
 def test_add():
     """Test the add function."""
     from src.utils.testfunctions import add
+    # use pytest to assert the result
     assert add(1, 2) == 3
+    assert add(10, 222) == 232
+    # test that add fails when the answer is wrong
+    with pytest.raises(AssertionError):
+        assert add(1, 2) == 4
+    # test that add fails when the arguments are not integers
+    pytest.raises(TypeError, add, "1", "2")
+    pytest.raises(TypeError, add, 1, "2")
+    pytest.raises(TypeError, add, "Spam", "Eggs")
+    
