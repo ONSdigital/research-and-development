e847cc9b07b984365123b2e6aa286b9e6a867fc3
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-01 19:57:12 +0000

    Uncommented run_pipeline and corrected where 'add' import pointed to

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 """The main pipeline"""
 
-#from src.utils.testfunctions import add
 import toml
+from utils.testfunctions import add
 
 def toml_parser():
     """Parse the toml config file"""
     return print( toml.load("research-and-development/config/userconfig.toml") )
 
-#def run_pipeline():
-#    """Run the pipeline"""
-#    return add(1, 2)
+def run_pipeline():
+    """Run the pipeline"""
+    return add(1, 2)
