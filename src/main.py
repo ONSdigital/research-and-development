3c9740fe104cd048eb54d63a3c60b68a339d0b47
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-01 17:21:40 +0000

    Printing the parsed toml as dict

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 """The main pipeline"""
 
-from src.utils.testfunctions import add
+#from src.utils.testfunctions import add
+import toml
+
+print( toml.load("research-and-development/config/userconfig.toml") )
 
 
-def run_pipeline():
-    """Run the pipeline"""
-    return add(1, 2)
+
+
+#def run_pipeline():
+#    """Run the pipeline"""
+#    return add(1, 2)
+
