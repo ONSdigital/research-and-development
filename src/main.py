cd41c9217d51ce3f815d3726f17596588ff14d52
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-13 19:03:23 +0000

    toml_parser() now contains a fully descriptive docstring

--- src/main.py
+++ src/main.py
@@ -4,7 +4,21 @@ import toml
 from utils.testfunctions import add
 
 def toml_parser():
-    """Parse the toml config file"""
+    """Function to parse the userconfig.toml file.
+
+    Returns:
+        A dictionary where the keys are section titles within the TOML file.
+        If only one variable under the section title in the TOML file is given
+        then it is passed directly as a dictionary value. If more than one
+        variable is defined then they are parsed as a dictionary themselves.
+        An example of what is returned is given below:
+        
+        {'title': 'TOML Example config', 'period': {'start_period': 
+        datetime.date(1990, 10, 10), 'end_period': datetime.date(2000, 10, 5)}, 
+        'source_file': {'location': 'D:/Data', 'fileName': 'file.txt'}, 'output_location': 
+        {'hive_db': 'hive.db', 'tableName': 'name.table'}, 'outlier_correction': 
+        {'location': 'D:/', 'fileName': 'outliers.txt', 'bool': True}}
+    """    
     return toml.load("/home/cdsw/research-and-development/config/userconfig.toml")
 
 def run_pipeline():
