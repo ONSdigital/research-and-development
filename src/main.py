e008d287642e16bdcd50438e84694205071d15fa
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-15 16:46:17 +0000

    Added functionality for easier imports of utility functions. Corrected typing import issue to include 'Callable' tpye hint.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 """The main pipeline"""
 
-from utils.testfunctions import add
+# Adds folder with utility function to path for easy imports
+import sys
+sys.path.append('/home/cdsw/research-and-development/src/utils')
 
-def run_pipeline() -> callable[[int,int], int]:
+import helpers
+from testfunctions import add
+
+# Importing from typing allows type hinting like Callable[[x,y],z]
+from typing import *
+
+def run_pipeline() -> Callable[[int,int], int]:
     """Run the pipeline"""
     return add(1, 2)
