4f5b94dd2a7b5c44365c6db5c31083cad70a7519
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-16 15:10:08 +0000

    Moved toml_parser() function and user_config_path to helpers.py.

--- src/main.py
+++ src/main.py
@@ -1,27 +1,6 @@
 """The main pipeline"""
 
-import toml
 from utils.testfunctions import add
-
-user_config_path = "config/userconfig.toml"
-
-
-def toml_parser(configfile: str = user_config_path) -> dict:
-    """Function to parse the userconfig.toml file.
-
-    Returns:
-        A nested dictionary where the keys are section titles within the TOML file.
-        If only one variable under the section title in the TOML file is given
-        then it is passed directly as a dictionary value. If more than one
-        variable is defined then they are parsed as a dictionary themselves.
-        An example of what is returned is given below:
-
-        {'title': 'TOML Example config', 'period': {'start_period':
-        datetime.date(1990, 10, 10), 'end_period': datetime.date(2000, 10, 5)}}
-    """
-    toml_dict = toml.load(configfile)
-
-    return toml_dict
 
 
 def run_pipeline():
