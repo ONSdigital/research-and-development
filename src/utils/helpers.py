36464cb1381e80ef5ed574d58493f7468d50c048
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-16 14:58:15 +0000

    Added helpers.py from 42_Combine_Loggers branch

---
+++ src/utils/helpers.py
@@ -0,0 +1,31 @@
+"""Define helper functions that wrap regularly-used functions."""
+
+import yaml
+import os
+
+
+class Config_settings:
+    """Get the config settings from the config file."""
+
+    def __init__(self):
+        self.config_file = "src/developer_config.yaml"
+        self.config_dict = self._get_config_settings()
+
+    def _get_config_settings(self):
+        """Get the config settings from the config file."""
+        with open(self.config_file, "r") as file:
+            config = yaml.safe_load(file)
+        return config
+
+
+def csv_creator(filename, columns):
+    """Creates a csv file with user
+    defined headers.
+    Args:
+        filename (string): Example: "name_of_file.csv"
+        columns (list): Example: ["a","b","c","d"]
+    """
+    if not os.path.exists(filename):
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(",".join(columns) + "\n")
+    return None
