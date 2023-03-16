6a050fe019351feefd069dfd335cdfd6d6b95c22
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-16 13:23:19 +0000

    Formatting updated. Added utils folder to path for better import functionality

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 """The main pipeline"""
 
-# Adds folder with utility function to path for easy imports
+# Importing from typing allows type hinting like Callable[[x,y],z]
 import sys
-sys.path.append('/home/cdsw/research-and-development/src/utils')
+import typing
 
-import helpers
 from testfunctions import add
 
-# Importing from typing allows type hinting like Callable[[x,y],z]
-from typing import *
+# Adds folder with utility function to path for easy imports
+sys.path.append("/home/cdsw/research-and-development/src/utils")
 
-def run_pipeline() -> Callable[[int,int], int]:
+
+def run_pipeline() -> typing.Callable[[int, int], int]:
     """Run the pipeline"""
     return add(1, 2)
