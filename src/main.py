b394cbde468d44cb66a5190aa97a5426fab6b297
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-21 17:11:34 +0000

    Removed toml parsing functions from main.py and added them to utils/helpers.py.

--- src/main.py
+++ src/main.py
@@ -1,39 +1,7 @@
 """The main pipeline"""
 
-import toml
 from utils.testfunctions import add
 
-def toml_parser():
-    """Function to parse the userconfig.toml file.
-
-    Returns:
-        A dictionary where the keys are section titles within the TOML file.
-        If only one variable under the section title in the TOML file is given
-        then it is passed directly as a dictionary value. If more than one
-        variable is defined then they are parsed as a dictionary themselves.
-        An example of what is returned is given below:
-        
-        {'title': 'TOML Example config', 'period': {'start_period': 
-        datetime.date(1990, 10, 10), 'end_period': datetime.date(2000, 10, 5)}, 
-        'source_file': {'location': 'D:/Data', 'fileName': 'file.txt'}, 'output_location': 
-        {'hive_db': 'hive.db', 'tableName': 'name.table'}, 'outlier_correction': 
-        {'location': 'D:/', 'fileName': 'outliers.txt', 'bool': True}}
-    """    
-    return toml.load("/home/cdsw/research-and-development/config/userconfig.toml")
-
-def period_select():
-    """Function returning the start and end date under consideration.
-
-    Returns:
-        A tuple containing two datetime.date objects. The first is the
-        start date of the period under consideration, the second is the
-        end date of that period. 
-        Example:
-    
-        (datetime.date(1990, 10, 10), datetime.date(2000, 10, 5))
-    """    
-    period_dict = toml_parser()["period"]
-    return period_dict["start_period"], period_dict["end_period"]
 
 def run_pipeline():
     """Run the pipeline"""
