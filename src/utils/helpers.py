d3fee8e349d6dc086203b5854361ac12f5fde47f
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-03-29 17:44:28 +0100

    Added comments and updated config imports

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -29,9 +29,14 @@ def hdfs_csv_creator(filepath, columns):
         filename (string): Example: "name_of_file.csv"
         columns (list): Example: ["a","b","c","d"]
     """
+
+    # Check if the file exists
     if not hdfs.path.isfile(filepath):
+        # open the file in write mode inside Hadoop context
         with hdfs.open(filepath, "wt") as file:
+            # Create new csv file in specified folder
             writer = csv.writer(file)
+            # Add the headers to the new csv
             writer.writerow(columns)
 
     return None
@@ -44,11 +49,17 @@ def hdfs_append(filepath, last_run):
         filepath (string): The filepath in Hue
         last_run (Dataframe): Dataframe of latest run data
     """
+
+    # Open the file in read mode inside Hadoop context
     with hdfs.open(filepath, "r") as file:
+        # Import csv file and convert to Dataframe
         df_imported_from_hdfs = pd.read_csv(file)
+        # Append new data
         last_run_data = df_imported_from_hdfs.append(last_run)
 
+    # Open the same file in write mode
     with hdfs.open(filepath, "wt") as file:
+        # Write new updated dataframe to DAP context
         last_run_data.to_csv(file, index=False)
 
     return None
