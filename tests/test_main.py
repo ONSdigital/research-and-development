9d9008e842c7148ebee06c585843fdeac95692fa
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-30 16:22:43 +0100

    Added skeletons for unit tests for user_config_reader and period_select functions.

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,3 +22,31 @@ def test_add():
     pytest.raises(TypeError, add, "1", "2")
     pytest.raises(TypeError, add, 1, "2")
     pytest.raises(TypeError, add, "Spam", "Eggs")
+
+
+def test_user_config_reader():
+    """Test the user_config_reader function."""
+    # Arrange
+    from src.utils.helpers import user_config_reader
+
+    # Act: use pytest to assert the result
+    result_1 = user_config_reader()
+    # Assert
+    assert type(result_1) == dict
+    # Assert: Negative test. Should fails when the answer is wrong
+
+    # Assert: test that add fails when the arguments are not integers
+
+
+def test_period_select():
+    """Test the preiod_select function."""
+    # Arrange
+    from src.utils.helpers import period_select
+
+    # Act: use pytest to assert the result
+    result_1 = period_select()
+    # Assert
+    assert type(result_1) == tuple
+    # Assert: Negative test. Should fails when the answer is wrong
+
+    # Assert: test that add fails when the arguments are not integers
