8f8aad4efb8c9d375796b02b4f142d56c6d8909b
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-03-30 10:37:59 +0100

    Deleting old config settings class

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -8,21 +8,6 @@ import os
 user_config_path = "config/userconfig.toml"
 
 
-class Config_settings:
-    """Get the config settings from the config file."""
-
-    def __init__(self):
-        self.config_file = "src/developer_config.yaml"
-        self.config_dict = self._get_config_settings()
-
-    def _get_config_settings(self):
-        """Get the config settings from the config file."""
-        with open(self.config_file, "r") as file:
-            config = yaml.safe_load(file)
-
-        return config
-
-
 def csv_creator(filename, columns):
     """Creates a csv file with user
     defined headers.
