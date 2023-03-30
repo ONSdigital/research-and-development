76ae8716fea1f6a77e363839957df4de253c77c4
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-30 10:26:57 +0100

    Added Args and description to docstring of user_config_reader function.

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -40,9 +40,12 @@ user_config_path = "config/userconfig.toml"
 def user_config_reader(configfile: str = user_config_path) -> dict:
     """Function to parse the userconfig.toml file.
 
+    Args:
+        configfile (str, optional): _description_. Defaults to user_config_path.
+
     Returns:
-        A nested dictionary where the keys are section titles within the TOML file.
-        If only one variable under the section title in the TOML file is given
+        dict: A nested dictionary where the keys are section titles within the TOML
+        file. If only one variable under the section title in the TOML file is given
         then it is passed directly as a dictionary value. If more than one
         variable is defined then they are parsed as a dictionary themselves.
         An example of what is returned is given below:
