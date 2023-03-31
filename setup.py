f56f28387cc5f354ed9f081ba767f5bc7515ca74
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-03-31 14:57:41 +0100

    Copied setup.py code from pub_sec project.

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup script for creating package from code."""
+from setuptools import setup, find_packages
+import re
+
+# Specify and open the version file
+VERSION_FILE = "pub_sec/_version.py"
+verstrline = open(VERSION_FILE, "rt").read()
+print(verstrline)
+
+# Automatically detect the package version from VERSION_FILE
+VERSION_REGEX = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VERSION_REGEX, verstrline, re.M)
+if mo:
+    version_string = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSION_FILE,))
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pub_sec",
+    version=version_string,
+    description="Public Sector local Python downloads and preprocessing package",
+    url="https://gitlab-app-l-01/EPDS/pub_sec",
+    packages=find_packages(),
+    package_data={"": ["*.toml", "*.r", "*.R", "*.pem"]},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
+)
