95987e9fc27d81ab1958564263e090138240f663
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-04-19 17:51:03 +0100

    Put code into a function

--- yml_to_requirements_convert.py
+++ yml_to_requirements_convert.py
@@ -1,24 +1,25 @@
 import yaml
 
-# yaml = ruamel.yaml.YAML()
-yml_env = yaml.safe_load(open("./environment.yml"))
 
-requirements = []
-for dep in yml_env["dependencies"]:
-    if isinstance(dep, str):
-        if "=" in dep:
-            package, package_version = dep.split("=")
-            if package == "python":
-                requirements.append(package + "==3.6.2")
+def yml_convertor():
+
+    yml_env = yaml.safe_load(open("./environment.yml"))
+
+    requirements = []
+    for dep in yml_env["dependencies"]:
+        if isinstance(dep, str):
+            if "=" in dep:
+                package, package_version = dep.split("=")
+                if package == "python":
+                    requirements.append(package + "==3.6.2")
+                else:
+                    requirements.append(package + "==" + package_version)
             else:
-                requirements.append(package + "==" + package_version)
-        else:
-            requirements.append(dep)
-    elif isinstance(dep, dict):
-        for preq in dep.get("pip", []):
-            requirements.append(preq)
-    print(requirements)
+                requirements.append(dep)
+        elif isinstance(dep, dict):
+            for preq in dep.get("pip", []):
+                requirements.append(preq)
 
-with open("./requirements.txt", "w") as fp:
-    for requirement in requirements:
-        print(requirement, file=fp)
+    with open("./requirements.txt", "w") as fp:
+        for requirement in requirements:
+            print(requirement, file=fp)
