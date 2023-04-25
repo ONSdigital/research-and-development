72289686c40b548ec196370f8dcb4948397fc3d2
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-04-25 18:10:22 +0100

    Removed unit test for check_file_exists() function to add it to its own file.

--- tests/test_main.py
+++ tests/test_main.py
@@ -49,17 +49,3 @@ def test_period_select():
     # Assert: Negative test. Should fails when the answer is wrong
     with pytest.raises(AssertionError):
         assert not isinstance(result_1, tuple)
-
-
-def test_check_file_exists():
-    """Test the check_file_exists function."""
-    # Arrange
-    from src.data_validation import check_file_exists
-
-    # Act: use pytest to assert the result
-    result_1 = check_file_exists()
-    # Assert
-    assert isinstance(result_1, bool)
-    # Assert: Negative test. Should fails when the answer is wrong
-    with pytest.raises(AssertionError):
-        assert not isinstance(result_1, bool)
