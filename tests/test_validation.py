18c18d913d0a97da235c32ade29ece795cb1326a
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-04-25 18:21:23 +0100

    Added True and False assertion tests in test_validation.py.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -9,9 +9,18 @@ def test_check_file_exists():
     from src.data_validation import check_file_exists
 
     # Act: use pytest to assert the result
+    empty_file = open("emptyfile.py", "a").close()
+
     result_1 = check_file_exists()
+    result_2 = check_file_exists("Non_existant_file.txt")
+    result_3 = check_file_exists(empty_file)
+
     # Assert
     assert isinstance(result_1, bool)
+    assert result_1
+    assert not result_2
+    assert not result_3
+
     # Assert: Negative test. Should fails when the answer is wrong
     with pytest.raises(AssertionError):
         assert not isinstance(result_1, bool)
