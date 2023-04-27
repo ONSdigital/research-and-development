b6441b833b3be15d27d4a8722fa3d61e051302f4
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-04-27 15:33:56 +0100

    made outputs clear

--- src/data_ingest/loading.py
+++ src/data_ingest/loading.py
@@ -37,4 +37,6 @@ def hdfs_load_json(filepath: str):
 
 snapdata, contributerdict, responsesdict = hdfs_load_json(file_path)
 
-print(contributerdict, responsesdict)
+print(contributerdict)
+print("\n")
+print(responsesdict)
