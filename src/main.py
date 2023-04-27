c9997f3004dd54af3ddd893afa60cab13d410689
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-04-27 13:33:00 +0100

    Had to comment out several lines in main as functions missing, including Config_settings. Branch cloned from develop.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 """The main pipeline"""
 
-from src.utils import runlog
-from src._version import __version__ as version
-from src.utils.helpers import Config_settings
-from src.utils.wrappers import logger_creator
+# from src.utils import runlog
+# from src._version import __version__ as version
+
+# from src.utils.helpers import Config_settings
+# from src.utils.wrappers import logger_creator
 from src.utils.testfunctions import Manipulate_data
-import time
+
+# import time
 import logging
 
 
@@ -21,25 +23,24 @@ def run_pipeline(start):
         generated from the time module using time.time()
     """
 
-    conf_obj = Config_settings()
-    config = conf_obj.config_dict
-    global_config = config["global"]
+    #    conf_obj = Config_settings()
+    #    config = conf_obj.config_dict
+    #    global_config = config["global"]
 
-    runlog_obj = runlog.RunLog(config, version)
+    # runlog_obj = runlog.RunLog(config, version)
 
-    logger = logger_creator(global_config)
+    #    logger = logger_creator(global_config)
     MainLogger.info("Launching Pipeline .......................")
-    logger.info("Collecting logging parameters ..........")
+    #    logger.info("Collecting logging parameters ..........")
     Manipulate_data()
     MainLogger.info("Finishing Pipeline .......................")
 
-    runlog_obj.retrieve_pipeline_logs()
+    # runlog_obj.retrieve_pipeline_logs()
 
-    run_time = round(time.time() - start, 5)
-    runlog_obj._record_time_taken(run_time)
+    # run_time = round(time.time() - start, 5)
+    # runlog_obj._record_time_taken(run_time)
 
-    runlog_obj.retrieve_configs()
-    runlog_obj._create_runlog_dicts()
-    runlog_obj._create_runlog_dfs()
-    runlog_obj.create_runlog_files()
-    runlog_obj._write_runlog()
+    # runlog_obj._create_runlog_dicts()
+    # runlog_obj._create_runlog_dfs()
+    # runlog_obj.create_runlog_files()
+    # runlog_obj._write_runlog()
