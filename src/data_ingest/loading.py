93484c758d3665cf5394e23bab8f117376628950
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-05-02 13:10:28 +0100

    Sorted import issues and added configs

--- src/data_ingest/loading.py
+++ src/data_ingest/loading.py
@@ -2,19 +2,12 @@ import pydoop.hdfs as hdfs
 import pandas as pd
 import json
 
-# from src.utils.helpers import Config_settings
+from src.utils.helpers import Config_settings
 
 
-# conf_obj = Config_settings()
-# config = conf_obj.config_dict
-# context = os.getenv("HADOOP_USER_NAME")  # Put your context name here
-# project = config["logs_foldername"]  # Taken from config file
-# file_name = "Loadin"
-# main_path = f"/user/{context}/{project}"
-# hdfs.mkdir(main_path)
-file_path = (
-    "/ons/rdbe_dev/snapshot-202012-002-fba5c4ba-fb8c-4a62-87bb-66c725eea5fd.json"
-)
+conf_obj = Config_settings()
+config = conf_obj.config_dict
+snapshot_path = config["snapshot_path"]  # Taken from config file
 
 
 def hdfs_load_json(filepath: str):
@@ -35,7 +28,7 @@ def hdfs_load_json(filepath: str):
     return datadf, contributerdict, responsesdict
 
 
-snapdata, contributerdict, responsesdict = hdfs_load_json(file_path)
+snapdata, contributerdict, responsesdict = hdfs_load_json(snapshot_path)
 
 print(contributerdict)
 print("\n")
