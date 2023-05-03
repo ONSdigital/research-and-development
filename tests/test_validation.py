b577c6f6198517be245068738d38c1cad66dfc22
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-05-03 15:02:57 +0100

    Added unit test for load_schema function in validation.py

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,8 +3,8 @@
 import pytest
 
 
-def test_add():
-    """Test the add function."""
+def test_check_data_shape():
+    """Test the check_data_shape function."""
     # Arrange
     from src.data_validation.validation import check_data_shape
 
@@ -20,3 +20,21 @@ def test_add():
     pytest.raises(TypeError, check_data_shape, 1, "2", 3)
     pytest.raises(TypeError, check_data_shape, "1", 2, 3)
     pytest.raises(TypeError, check_data_shape, "1", "2", "3")
+
+
+def test_load_schema():
+    """Test the load_schema function."""
+    # Arrange
+    from src.data_validation.validation import load_schema
+
+    # Act: use pytest to assert the result
+    result_1 = load_schema()
+
+    # Assert
+    assert isinstance(result_1, dict)
+    # Assert: Negative test. Should fails when the answer is wrong
+    with pytest.raises(AssertionError):
+        assert not isinstance(result_1, dict)
+    # Assert: test that add fails when the arguments are wrong type
+    pytest.raises(TypeError, load_schema, 2)
+    pytest.raises(TypeError, load_schema, True)
