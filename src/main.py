8ec73844f042490f976f023cb1ee4a8b794bd4ec
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-05-15 18:27:54 +0100

    Reverted src/main.py to have no commented lines.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 """The main pipeline"""
 
-# from src.utils import runlog
-# from src._version import __version__ as version
+from src.utils import runlog
+from src._version import __version__ as version
 
-# from src.utils.helpers import Config_settings
-# from src.utils.wrappers import logger_creator
+from src.utils.helpers import Config_settings
+from src.utils.wrappers import logger_creator
 from src.utils.testfunctions import Manipulate_data
 from src.data_ingest import spp_parser
 from src.data_processing import spp_snapshot_processing as processing
@@ -33,9 +33,9 @@ def run_pipeline(start):
     global_config = config["global"]
     runlog_obj = runlog.RunLog(config, version)
 
-    #    logger = logger_creator(global_config)
+    logger = logger_creator(global_config)
     MainLogger.info("Launching Pipeline .......................")
-    #    logger.info("Collecting logging parameters ..........")
+    logger.info("Collecting logging parameters ..........")
     Manipulate_data()
 
     # Data Ingest
@@ -79,12 +79,12 @@ def run_pipeline(start):
 
     MainLogger.info("Finishing Pipeline .......................")
 
-    # runlog_obj.retrieve_pipeline_logs()
+    runlog_obj.retrieve_pipeline_logs()
 
-    # run_time = round(time.time() - start, 5)
-    # runlog_obj._record_time_taken(run_time)
+    run_time = round(time.time() - start, 5)
+    runlog_obj._record_time_taken(run_time)
 
-    # runlog_obj._create_runlog_dicts()
-    # runlog_obj._create_runlog_dfs()
-    # runlog_obj.create_runlog_files()
-    # runlog_obj._write_runlog()
+    runlog_obj._create_runlog_dicts()
+    runlog_obj._create_runlog_dfs()
+    runlog_obj.create_runlog_files()
+    runlog_obj._write_runlog()
