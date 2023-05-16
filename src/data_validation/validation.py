eade13cf5d11c504a29a3e399ab55bfe05c6fab9
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-05-16 21:24:18 +0100

    Creating func to validate whole col

--- src/data_validation/validation.py
+++ src/data_validation/validation.py
@@ -1,4 +1,5 @@
 import postcodes_uk
+import pandas as pd
 
 # import pandera
 
@@ -16,3 +17,19 @@ def validate_postcode(pcode: str) -> bool:
     valid_bool = postcodes_uk.validate(pcode)
 
     return valid_bool
+
+
+def validate_post_col(df: pd.Dataframe) -> bool:
+    """_summary_
+
+    Args:
+        df (pd.Dataframe): _description_
+
+    Returns:
+        bool: _description_
+    """
+    bool_series = df.referencepostcode.apply(validate_postcode)
+
+    whole_col_valid = bool_series.all()
+
+    return whole_col_valid
