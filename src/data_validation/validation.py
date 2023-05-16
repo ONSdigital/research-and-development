832f667e09514b556c5bc379e63a691e6df0e23d
jwestw <james.westwood@ons.gov.uk>
2023-05-16 21:45:39 +0100

    Including error  of invalid postcodes on failure

--- src/data_validation/validation.py
+++ src/data_validation/validation.py
@@ -20,16 +20,28 @@ def validate_postcode(pcode: str) -> bool:
 
 
 def validate_post_col(df: pd.Dataframe) -> bool:
-    """_summary_
+    """This function checks if all postcodes in the specified DataFrame column 
+        are valid UK postcodes. It uses the `validate_postcode` function to 
+        perform the validation.
 
     Args:
-        df (pd.Dataframe): _description_
+        df (pd.DataFrame): The DataFrame containing the postcodes.
 
     Returns:
-        bool: _description_
+        bool: True if all postcodes are valid, False otherwise.
+
+    Raises:
+        ValueError: If any invalid postcodes are found, a ValueError is raised.
+            The error message includes the list of invalid postcodes.
+
+    Example:
+        >>> df = pd.DataFrame({"referencepostcode": ["AB12 3CD", "EFG 456", "HIJ 789", "KL1M 2NO"]})
+        >>> validate_post_col(df)
+        ValueError: Invalid postcodes found: ['EFG 456', 'HIJ 789']
     """
-    bool_series = df.referencepostcode.apply(validate_postcode)
+    invalid_postcodes = df.loc[~df["referencepostcode"].apply(validate_postcode), "referencepostcode"]
 
-    whole_col_valid = bool_series.all()
-
-    return whole_col_valid
+    if not invalid_postcodes.empty:
+        raise ValueError(f"Invalid postcodes found: {invalid_postcodes.to_list()}")
+    
+    return True
