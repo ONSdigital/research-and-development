e3c1de6b5f358b3fe261ebdd0b54e753a816f7b9
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-05-17 14:27:41 +0100

    Creating test folder structure

--- tests/data_validation/test_validation.py
+++ tests/data_validation/test_validation.py
@@ -1,15 +1,12 @@
-import pytest
-from src.data_validation import validate_postcode
-
-
-import pytest
 import pandas as pd
-from your_module import validate_post_col
+import pytest
+from src.data_validation import validate_postcode, validate_post_col
+
 
 def test_validate_post_col():
     # Valid postcodes
     df_valid = pd.DataFrame({"referencepostcode": ["AB12 3CD", "DE34 5FG", "HI67 8JK"]})
-    assert validate_post_col(df_valid) == True
+    assert validate_post_col(df_valid)
 
     # Invalid postcodes
     df_invalid = pd.DataFrame({"referencepostcode": ["EFG 456", "HIJ 789"]})
@@ -18,13 +15,19 @@ def test_validate_post_col():
     assert str(error.value) == "Invalid postcodes found: ['EFG 456', 'HIJ 789']"
 
     # Mixed valid and invalid postcodes
-    df_mixed_valid_invalid = pd.DataFrame({"referencepostcode": ["AB12 3CD", "EFG 456", "HI67 8JK"]})
+    df_mixed_valid_invalid = pd.DataFrame(
+        {"referencepostcode": ["AB12 3CD", "EFG 456", "HI67 8JK"]}
+    )
     with pytest.raises(ValueError) as error:
         validate_post_col(df_mixed_valid_invalid)
-    assert str(error.value) == "Invalid postcodes found: ['EFG 456']"  # Mixed valid and invalid postcodes
+    assert (
+        str(error.value) == "Invalid postcodes found: ['EFG 456']"
+    )  # Mixed valid and invalid postcodes
 
     # Edge cases: invalid column names
-    df_invalid_column_name = pd.DataFrame({"postcode": ["AB12 3CD", "EFG 456", "HI67 8JK"]})
+    df_invalid_column_name = pd.DataFrame(
+        {"postcode": ["AB12 3CD", "EFG 456", "HI67 8JK"]}
+    )
     with pytest.raises(KeyError) as error:
         validate_post_col(df_invalid_column_name)
     assert str(error.value) == "'referencepostcode'"  # Invalid column name
@@ -42,26 +45,25 @@ def test_validate_post_col():
 
     # Edge cases: empty reference postcode column
     df_no_postcodes = pd.DataFrame({"referencepostcode": [""]})
-    assert validate_post_col(df_no_postcodes) == False  # Empty postcode
+    assert validate_post_col(df_no_postcodes) is False  # Empty postcode
 
 
 def test_validate_postcode():
     # Valid postcodes
-    assert validate_postcode("AB12 3CD") == True
-    assert validate_postcode("DE34 5FG") == True
-    assert validate_postcode("HI67 8JK") == True
+    assert validate_postcode("AB12 3CD") is True
+    assert validate_postcode("DE34 5FG") is False
+    assert validate_postcode("HI67 8JK") is True
 
     # Invalid postcodes
-    assert validate_postcode("EFG 456") == False
-    assert validate_postcode("HIJ 789") == False
-    assert validate_postcode("KL1M 2NO") == False
-    assert validate_postcode("B27 OAG") == False  # Zero is actually an "O"
+    assert validate_postcode("EFG 456") is False
+    assert validate_postcode("HIJ 789") is False
+    assert validate_postcode("KL1M 2NO") is False
+    assert validate_postcode("B27 OAG") is False  # Zero is actually an "O"
 
     # Edge cases
-    assert validate_postcode(None) == False  # None value should fail
-    assert validate_postcode("") == False  # Empty string
-    assert validate_postcode(" ") == False  # Whitespace
-    assert validate_postcode("AB123CD") == False  # Missing space - othewise valid
-    assert validate_postcode("ABC XYZ") == False  # All letters but right length
-    assert validate_postcode("123 456") == False  # All numbers but right length
-
+    assert validate_postcode(None) is False  # None value should fail
+    assert validate_postcode("") is False  # Empty string
+    assert validate_postcode(" ") is False  # Whitespace
+    assert validate_postcode("AB123CD") is False  # Missing space - othewise valid
+    assert validate_postcode("ABC XYZ") is False  # All letters but right length
+    assert validate_postcode("123 456") is False  # All numbers but right length
