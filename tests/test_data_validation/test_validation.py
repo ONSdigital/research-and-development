9d3ffaf7bdd46ab5b9e83b25ac488cf844fe3738
jwestw <james.westwood@ons.gov.uk>
2023-05-23 11:38:07 +0100

    Mock loading masterlist. Testing logs & errors

--- tests/test_data_validation/test_validation.py
+++ tests/test_data_validation/test_validation.py
@@ -3,7 +3,30 @@ import pytest
 from src.data_validation.validation import validate_postcode, validate_post_col
 
 
-def test_validate_post_col():
+# Define test data
+@pytest.fixture
+def test_data():
+    return pd.DataFrame({"referencepostcode": ["AB12 3CD", "EFG 456", "HIJ 789", "KL1M 2NO"]})
+
+# Mock the get_masterlist function
+def mock_get_masterlist(masterlist_path):
+    # Return a mock masterlist series
+    return pd.Series(["AB12 3CD", "KL1M 2NO"])
+
+
+# Test case for validate_post_col
+def test_validate_post_col(test_data, monkeypatch, caplog):
+    # Monkeypatch the get_masterlist function to use the mock implementation
+    monkeypatch.setattr("src.data_validation.validation.get_masterlist", mock_get_masterlist)
+
+    # Call the function under test
+    with pytest.raises(ValueError) as exc_info:
+        validate_post_col(test_data, "path/to/missing_masterlist.csv")
+
+    # Using caplog to check the logged warning messages
+    assert "These postcodes are not found in the ONS postcode list: ['EFG 456', 'HIJ 789']" in caplog.text
+    assert "Invalid postcodes found: ['EFG 456', 'HIJ 789']" in caplog.text
+
     # Valid postcodes
     df_valid = pd.DataFrame({"referencepostcode": ["AB12 3CD", "DE34 5FG", "HI67 8JK"]})
     assert validate_post_col(df_valid)
@@ -20,9 +43,7 @@ def test_validate_post_col():
     )
     with pytest.raises(ValueError) as error:
         validate_post_col(df_mixed_valid_invalid)
-    assert (
-        str(error.value) == "Invalid postcodes found: ['EFG 456']"
-    )  # Mixed valid and invalid postcodes
+    assert (str(error.value) == "Invalid postcodes found: ['EFG 456']")
 
     # Edge cases: invalid column names
     df_invalid_column_name = pd.DataFrame(
