77e2c9bd13f0312aec2017b0303461cbea728ca7
Roddy Macrae <Roddy.Macrae@ext.ons.gov.uk>
2023-05-25 16:03:53 +0100

    Updated loading.py to latest version

--- src/data_ingest/loading.py
+++ src/data_ingest/loading.py
@@ -20,28 +20,3 @@ print("\n")
 print(responses.head())
 print("\n")
 print([responses["questioncode"].unique()])
-import pydoop.hdfs as hdfs
-import pandas as pd
-import json
-
-file_path = (
-    "/ons/rdbe_dev/snapshot-202012-002-fba5c4ba-fb8c-4a62-87bb-66c725eea5fd.json"
-)
-
-
-def hdfs_load_json(filepath: str):
-    """Function to load JSON data from DAP
-    Args:
-        filepath (string): The filepath in Hue
-    """
-
-    # Open the file in read mode inside Hadoop context
-    with hdfs.open(filepath, "r") as file:
-        # Import csv file and convert to Dataframe
-        datadict = json.load(file)
-        contributerdict = datadict["contributors"][0]
-        responsesdict = datadict["responses"][0]
-
-    datadf = pd.DataFrame.from_dict(datadict, orient="index")
-
-    return datadf, contributerdict, responsesdict
