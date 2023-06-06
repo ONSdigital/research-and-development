e9c45ed36325db5bccb91379519a42e9f928ee69
Ilyas Ali <Ilyas.Ali@ext.ons.gov.uk>
2023-06-06 18:53:55 +0100

    Tests for loading.py module

--- src/data_ingest/loading.py
+++ src/data_ingest/loading.py
@@ -8,8 +8,10 @@ from src.utils.hdfs_mods import hdfs_load_json
 conf_obj = Config_settings()
 config = conf_obj.config_dict
 snapshot_path = config["paths"]["snapshot_path"]  # Taken from config file
+snapdata = hdfs_load_json(snapshot_path)
 
-def parse_snap_data(snapdata: dict = snapshot_path) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+def parse_snap_data(snapdata: dict = snapdata) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Loads the data from the survey via the SPP snapshot. The data is supplied as dict
         and is parsed into dataframes, one for survey contributers (company details)
         and another one for their responses.
@@ -21,7 +23,6 @@ def parse_snap_data(snapdata: dict = snapshot_path) -> Tuple[pd.DataFrame, pd.Da
         Tuple[pd.DataFrame, pd.DataFrame]: The contributers and responders dataframes
     """
     # Load the dicts
-    snapdata = hdfs_load_json(snapshot_path)
     contributordict = snapdata["contributors"]
     responsesdict = snapdata["responses"]
 
