26256ce12dfdfa832c704a5eb6a4ebd5d20f5dcb
jwestw <james.westwood@ons.gov.uk>
2023-06-07 10:27:47 +0100

    Adding tests for new functions

--- tests/test_data_processing/test_spp_snapshot_processing.py
+++ tests/test_data_processing/test_spp_snapshot_processing.py
@@ -76,3 +76,50 @@ def test_response_rate(dummy_data):
 
     expected_response_rate = 2 / 3  # 2 respondents out of 3 contributors
     assert expected_response_rate == response_rate_value
+
+
+def test_create_response_dataframe(dummy_data):
+    
+    from src.data_processing.spp_snapshot_processing import create_response_dataframe
+    
+    contributor_data, responses_data = dummy_data
+    unique_id_cols = ["reference", "period", "survey"]
+    expected_columns = ["reference", "period", "survey", 200, 201, 202]
+    expected_data = [
+        [101, 202012, 1, 0, 50, 100],
+        [102, 202012, 1, 75, 25, 65],
+    ]
+
+    response_df = create_response_dataframe(responses_data, unique_id_cols)
+
+    # Assert the columns
+    assert response_df.columns.tolist() == expected_columns
+
+    # Assert the data
+    assert response_df.values.tolist() == expected_data
+    
+def test_create_contextual_dataframe(dummy_data):
+    contributor_data, responses_data = dummy_data
+    unique_id_cols = ["reference", "period", "survey"]
+    expected_columns = [
+        "reference",
+        "period",
+        "survey",
+        "createdby",
+        "createddate",
+        "lastupdatedby",
+        "lastupdateddate",
+    ]
+    expected_data = [
+        [101, 202012, 1, "James", 2020, "Vondy", 2020],
+        [102, 202012, 1, "Ilyas", 2020, "Charl", 2020],
+        [103, 202012, 1, "Roddy", 2020, "Gareth", 2020],
+    ]
+
+    contextual_df = create_contextual_dataframe(contributor_data, unique_id_cols)
+
+    # Assert the columns
+    assert contextual_df.columns.tolist() == expected_columns
+
+    # Assert the data
+    assert contextual_df.values.tolist() == expected_data
