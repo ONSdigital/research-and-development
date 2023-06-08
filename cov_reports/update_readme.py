5492cad7dfd327ee5803e2fe84e2e3097b92b71f
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-06-08 13:35:05 +0100

    Auto-updating readme coverage badge

--- cov_reports/update_readme.py
+++ cov_reports/update_readme.py
@@ -2,3 +2,5 @@ from subprocess import run
 
 run(["python", "-m", "coverage", "run", "-m", "pytest"])
 run(["readme-cov"])
+run(["git", "add", "README.md"])
+run(["git", "commit", "-m", "Auto-updating readme coverage badge"])
