27979b895272c4fcb0a6be424655d506c92e1ab7
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-06-20 12:54:09 +0100

    Example path

--- src/staging/history_loader.py
+++ src/staging/history_loader.py
@@ -1,16 +1,17 @@
 """Functions relating to loading historic data"""
 
 
-
 def history_years(current, back_history):
     """Gets the year or years to load historic data for"""
-    
-    if back_history >0:
+
+    if back_history > 0:
         # create a generator to yield the years to load
         for i in range(back_history):
-            yield current -1
+            yield current - 1
             current -= 1
     else:
         print("No historic data to load")
         return None
 
+
+example_path = "hdfs://dnt01/ons/rdbe_dev/BERD_V7_Anonymised/cp_BERD_202012_cp3.csv"
