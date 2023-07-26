91dbb11fa868c2d15ec4a9bd084f414280ffccfa
okudum <Muttesir.Okuducu@ext.ons.gov.uk>
2023-07-26 14:13:40 +0100

    updated codes

--- src/data_ingest/check_data_type.py
+++ src/data_ingest/check_data_type.py
@@ -1,67 +1,38 @@
-# import toml
-# import pandas as pd
-# import logging
-
-# # from src.utils.wrappers import time_logger_wrap, exception_wrap
-
-# datatype_logger = logging.getLogger(__name__)
+import toml
+import pandas as pd
+import logging
 
 
-# def validate_json_shcema(data: pd.DataFrame, schema: str):
+# from src.utils.wrappers import time_logger_wrap, exception_wrap
 
-#     try:
+datatype_logger = logging.getLogger(__name__)
 
-#         with open(schema) as f:
-#             schema_dict = toml.load(f)
 
-#         print(data.dtypes)
-#         # print(data.info())
+def validate_json_shcema(data: pd.DataFrame, schema: str):
 
-#         for column, dtype in schema_dict.items():
-#             # print(f'column{column}:{dtype}')
-#             d_type = dtype.get("Deduced_Data_Type")
-#             print(d_type)
-#             if column not in data.columns:
-#                 raise ValueError(f"column{column} not in data frame")
+    try:
 
-#             d_type = dtype.get("Deduced_Data_Type")
+        with open(schema) as f:
+            schema_dict = toml.load(f)
 
-#             if d_type == "None":
-#                 data[column] = data[column].replace({"None": pd.NA})
+        # print(data.dtypes)
+        # print(data.info())
+    except FileNotFoundError:
+        print(f"The file  {schema} does not exist")
+        raise SystemExit(1)
 
-#             elif d_type == "Datetime":
-#                 data[column] = pd.to_datetime(data[column], errors="coerce")
+    except Exception as nested_exception:
+        print(f"Nested Exception: {nested_exception}")
 
-#             elif d_type == "pandas.NA":
-#                 data[column] = data[column].replace({"pandas.NA": pd.NA})
+        data_types_dict = {
+            column_nm: schema_dict[column_nm]["Deduced_Data_Type"]
+            for column_nm in schema_dict.keys()
+        }
+        # data.astype(data_types_dict)
+        for column in data.columns:
+            try:
+                data[column].astype(data_types_dict[column])
+                print(data.dtypes)
 
-#             elif d_type == "int":
-#                 data[column] = pd.to_numeric(data[column], errors="coerce").astype(
-#                     "Int64"
-#                 )
-
-#             elif d_type == "float":
-#                 data[column] = pd.to_numeric(data[column], errors="coerce")
-
-#             elif d_type == "category":
-#                 data[column] = data[column].astype(str)
-
-#             elif d_type == "str":
-#                 data[column] = data[column].astype(str)
-
-#             else:
-
-#                 raise ValueError(
-#                     f"Invalid data type {dtype}, specifed col {column}, in schema"
-#                 )
-
-#         return data
-
-#     except FileNotFoundError:
-#         print(f"The file  {schema} does not exist")
-#         raise SystemExit(1)
-
-#     except Exception as e:
-#         print("Schema validation failed for")
-#         print(e)
-#         raise SystemExit(1)
+            except toml.TomlDecodeError as e:
+                print(f"tomldecode error:{e}")
