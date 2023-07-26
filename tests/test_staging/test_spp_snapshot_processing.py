5297002eeb2b4924ea462fa1eda730c409a8553d
griffa1 <Anne.Griffith@ons.gov.uk>
2023-07-26 15:30:11 +0100

    pytests working for updated spp processing

--- tests/test_staging/test_spp_snapshot_processing.py
+++ tests/test_staging/test_spp_snapshot_processing.py
@@ -17,66 +17,78 @@ from src.staging.spp_snapshot_processing import (
 @pytest.fixture
 def dummy_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
     # Set up the dummy data
-    contributor_schema =   {
+    contributor_schema = {
         "reference": "int64",
-        "period": "int64", 
+        "period": "int64",
         "survey": "int64",
         "createdby": "str",
-        "createddate": "Int64", 
-        "lastupdatedby": "str", 
-        "lastupdateddate": "Int64", 
+        "createddate": "Int64",
+        "lastupdatedby": "str",
+        "lastupdateddate": "Int64",
     }
     contributor_data = [
         [101, 202012, 1, "James", 2020, "Vondy", 2020],
         [102, 202012, 1, "Ilyas", 2020, "George", 2020],
-        [103, 202012, 1, "Muttesir", 2020, "Anne", 2020]
+        [103, 202012, 1, "Muttesir", 2020, "Anne", 2020],
     ]
-    contributor_input = pd.DataFrame(data=contributor_data, columns=contributor_schema.keys()).astype(contributor_schema)
+    contributor_input = pd.DataFrame(
+        data=contributor_data, columns=contributor_schema.keys()
+    ).astype(contributor_schema)
 
-    responses_schema =  {
-            "reference": "int64",
-            "period": "int64",
-            "survey": "int64", 
-            "createdby": "str",
-            "createddate": "Int64", 
-            "lastupdatedby": "str",
-            "lastupdateddate": "Int64", 
-            "questioncode": "Int64",
-            "response": "Int64", 
-            "adjustedresponse": "str"
-        }
-    
+    responses_schema = {
+        "reference": "int64",
+        "instance": "float",
+        "period": "int64",
+        "survey": "int64",
+        "createdby": "str",
+        "createddate": "Int64",
+        "lastupdatedby": "str",
+        "lastupdateddate": "Int64",
+        "questioncode": "Int64",
+        "response": "Int64",
+        "adjustedresponse": "str",
+    }
+
     responses_data = [
-            [101, 202012, 1, "A", 2020, "A", 2020, 200, 0, ""],
-            [101, 202012, 1, "A", 2020, "A", 2020, 201, 50, ""],
-            [101, 202012, 1, "A", 2020, "A", 2020, 202, 100, ""],
-            [102, 202012, 1, "A", 2020, "A", 2020, 200, 75, ""],
-            [102, 202012, 1, "A", 2020, "A", 2020, 201, 25, ""],
-            [102, 202012, 1, "A", 2020, "A", 2020, 202, 65, ""]
+        [101, 0, 202012, 1, "A", 2020, "A", 2020, 200, 0, ""],
+        [101, 0, 202012, 1, "A", 2020, "A", 2020, 201, 50, ""],
+        [101, 0, 202012, 1, "A", 2020, "A", 2020, 202, 100, ""],
+        [101, 1, 202012, 1, "A", 2020, "A", 2020, 200, 10, ""],
+        [101, 1, 202012, 1, "A", 2020, "A", 2020, 201, 510, ""],
+        [101, 1, 202012, 1, "A", 2020, "A", 2020, 202, 110, ""],
+        [102, 0, 202012, 1, "A", 2020, "A", 2020, 200, 75, ""],
+        [102, 0, 202012, 1, "A", 2020, "A", 2020, 201, 25, ""],
+        [102, 0, 202012, 1, "A", 2020, "A", 2020, 202, 65, ""],
     ]
-    
-    responses_input = pd.DataFrame(data=responses_data, columns=responses_schema.keys()).astype(responses_schema)
+
+    responses_input = pd.DataFrame(
+        data=responses_data, columns=responses_schema.keys()
+    ).astype(responses_schema)
     return contributor_input, responses_input
 
 
 @pytest.fixture
 def expected_output():
-    expected_schema =   {
-        "reference": "int64", 
-        "period": "int64", 
-        "survey": "int64", 
-        200: "Int64", 
-        201: "Int64", 
-        202: "Int64"
-        }
-    
-    expected_data =  [
-        [101, 202012, 1, 0,      50, 100],
-        [102, 202012, 1, 75,     25, 65],
-        [103, 202012, 1, np.nan, np.nan, np.nan]
+    expected_schema = {
+        "reference": "int64",
+        "instance": "float",
+        200: "Int64",
+        201: "Int64",
+        202: "Int64",
+        "period": "int64",
+        "survey": "int64",
+    }
+
+    expected_data = [
+        [101, 0, 0, 50, 100, 202012, 1],
+        [101, 1, 10, 510, 110, 202012, 1],
+        [102, 0, 75, 25, 65, 202012, 1],
+        [103, np.nan, np.nan, np.nan, np.nan, 202012, 1],
     ]
-    
-    expected_output = pd.DataFrame(data=expected_data, columns=expected_schema.keys()).astype(expected_schema)
+
+    expected_output = pd.DataFrame(
+        data=expected_data, columns=expected_schema.keys()
+    ).astype(expected_schema)
 
     return expected_output
 
@@ -90,7 +102,9 @@ def test_full_responses(dummy_data, expected_output):
 
     df_result = full_responses(contributor_data, responses_data)
 
-    pd.testing.assert_frame_equal(df_result, expected_output_data)
+    pd.testing.assert_frame_equal(
+        df_result, expected_output_data, check_like=True, check_dtype=False
+    )
 
 
 def test_response_rate(dummy_data):
@@ -107,11 +121,12 @@ def test_response_rate(dummy_data):
 def test_create_response_dataframe(dummy_data):
 
     contributor_data, responses_data = dummy_data
-    unique_id_cols = ["reference", "period", "survey"]
-    expected_columns = ["reference", "period", "survey", 200, 201, 202]
+    unique_id_cols = ["reference", "instance"]
+    expected_columns = ["reference", "instance", 200, 201, 202]
     expected_data = [
-        [101, 202012, 1, 0, 50, 100],
-        [102, 202012, 1, 75, 25, 65],
+        [101, 0, 0, 50, 100],
+        [101, 1, 10, 510, 110],
+        [102, 0, 75, 25, 65],
     ]
 
     response_df = create_response_dataframe(responses_data, unique_id_cols)
@@ -125,9 +140,10 @@ def test_create_response_dataframe(dummy_data):
 
 def test_create_contextual_dataframe(dummy_data):
     contributor_data, responses_data = dummy_data
-    unique_id_cols = ["reference", "period", "survey"]
+    unique_id_cols = ["reference", "instance"]
     expected_columns = [
         "reference",
+        "instance",
         "period",
         "survey",
         "createdby",
@@ -137,8 +153,9 @@ def test_create_contextual_dataframe(dummy_data):
         "adjustedresponse",
     ]
     expected_data = [
-        [101, 202012, 1, "A", 2020, "A", 2020, ""],
-        [102, 202012, 1, "A", 2020, "A", 2020, ""],
+        [101, 0, 202012, 1, "A", 2020, "A", 2020, ""],
+        [101, 1, 202012, 1, "A", 2020, "A", 2020, ""],
+        [102, 0, 202012, 1, "A", 2020, "A", 2020, ""],
     ]
 
     contextual_df = create_contextual_dataframe(responses_data, unique_id_cols)
