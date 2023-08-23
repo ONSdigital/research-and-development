993f7db25e768a8656d3800fd6ecc47ed90981ab
Smith <vondy.smith@ons.gov.uk>
2023-08-23 10:46:33 +0100

    added functions to check for duplicate rows and return these with a count and stop pipeline

---
+++ src/estimation/duplicates_check.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import logging
+import sys
+
+DupLogger = logging.getLogger(__name__)
+
+def count_unique(df: pd.DataFrame) -> pd.DataFrame:
+    '''Returns dataframe with count column
+    giving count of unique row
+
+    Args:
+        df (pd.DataFrame): Pandas dataframe
+        with responses data
+
+    Returns:
+        duplicates(pd.DataFrame): Pandas dataframe
+        with duplicate rows or empty dataframe 
+        if no duplicates exist 
+    '''
+
+    # group by all columns and
+    # add col "count" with count of unique rows
+    duplicates = df.groupby(
+        df.columns.tolist()
+        ).size().reset_index().rename(columns={0:'count'})
+
+    return duplicates
+
+
+def duplicates_check(df: pd.DataFrame) -> pd.DataFrame:
+    '''Creates error message with duplicates
+    if they exist. If not gives an info message
+
+    Args:
+        df (pd.DataFrame): Pandas dataframe
+        with responses data
+    '''
+
+    # obtain counts of unique rows
+    # and filter for any not 1
+    duplicates = count_unique(df)
+    duplicates = duplicates[duplicates["count"] != 1]
+    
+    if len(duplicates) > 0:
+        DupLogger.error(
+            (
+                "The following dataframe shows duplicate rows "
+                "(see the count column for a count of duplicate rows)"
+                f": {duplicates}"
+
+            )
+        )
+
+        #exit pipeline if any duplicates
+        sys.exit()
+    else:
+        DupLogger.info(
+            (
+                f"There are no duplicate rows in the data"
+
+            )
+        )
---
+++ tests/test_estimation/test_duplicates_check.py
@@ -0,0 +1,92 @@
+from pandas._testing import assert_frame_equal
+from pandas import DataFrame as pandasDF
+
+from src.estimation.duplicates_check import count_unique
+
+class TestCountUnique:
+    """Unit tests for count_unique function."""
+
+    def create_input_df(self):
+        """Create an input dataframe for the test."""
+        input_cols = [
+            "reference",
+            "col1",
+            "col2"
+        ]
+
+        data = [
+            [1, 123, 123],
+            [1, 123, 123],
+            [2, 1234, 1234],
+        ]
+
+        input_df = pandasDF(data=data, columns=input_cols)
+        return input_df
+
+    def create_input_df_nodup(self):
+        """Create an input dataframe for the test."""
+        input_cols = [
+            "reference",
+            "col1",
+            "col2"
+        ]
+
+        data = [
+            [1, 123, 123],
+            [2, 1234, 1234],
+        ]
+
+        input_df = pandasDF(data=data, columns=input_cols)
+        return input_df
+
+    def create_expected_df(self):
+        """Create an input dataframe for the test."""
+        exp_cols = [
+            "reference",
+            "col1",
+            "col2",
+            "count"
+        ]
+
+        data = [
+            [1, 123, 123, 2],
+            [2, 1234, 1234, 1],
+        ]
+
+        exp_df = pandasDF(data=data, columns=exp_cols)
+        return exp_df
+
+    def create_expected_df_nodup(self):
+        """Create an input dataframe for the test."""
+        exp_cols = [
+            "reference",
+            "col1",
+            "col2",
+            "count"
+        ]
+
+        data = [
+            [1, 123, 123, 1],
+            [2, 1234, 1234, 1],
+        ]
+
+        exp_df = pandasDF(data=data, columns=exp_cols)
+        return exp_df
+
+    def test_count_unique(self):
+        """Test for count_unique function."""
+        input_df = self.create_input_df()
+        expected_df = self.create_expected_df()
+
+        result_df = count_unique(input_df)
+
+        assert_frame_equal(result_df, expected_df)
+
+    def test_count_unique_nodup(self):
+        """Test for count_unique function."""
+        input_df = self.create_input_df_nodup()
+        expected_df = self.create_expected_df_nodup()
+
+        result_df = count_unique(input_df)
+
+        assert_frame_equal(result_df, expected_df)
