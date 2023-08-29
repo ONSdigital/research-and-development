63b5f27ea12a50f11bce0783829932330e7ccf33
griffa1 <anne.griffith@ons.gov.uk>
2023-08-29 09:19:15 +0100

    improved short form docstrings

--- src/outputs/short_form_out.py
+++ src/outputs/short_form_out.py
@@ -5,11 +5,15 @@ import numpy as np
 def create_new_cols(df: pd.DataFrame) -> pd.DataFrame:
     """Created blank columns & year column for short form output
 
+    Blank columns are created for short form output that are required
+    but will currently be supplied empty.
+    The 'period_year' column is added containing the year in form 'YYYY'.
+
     Args:
-        df (pd.DataFrame): short form output data frame
+        df (pd.DataFrame): The main dataframe to be used for short form output.
 
     Returns:
-        pd.DataFrame: returns short form output data frame with added new columns
+        pd.DataFrame: returns short form output data frame with added new cols
     """
     columns_names = [
         "freeze_id",
