36b8fb1c48b5b7e4dcb7e8aef060df2d4f1040d6
jwestw <james.westwood@ons.gov.uk>
2023-09-11 17:10:26 +0100

    Creating manifest object with callable funcs

--- src/outputs/outputs_main.py
+++ src/outputs/outputs_main.py
@@ -6,6 +6,7 @@ from typing import Callable, Dict, Any
 
 from src.outputs.short_form_out import run_shortform_prep
 from src.outputs.temp_file_to_be_deleted import combine_dataframes
+from src.outputs.manifest_output import Manifest
 
 OutputMainLogger = logging.getLogger(__name__)
 
@@ -16,6 +17,13 @@ def run_output(
     write_csv: Callable,
     run_id: int,
     ultfoc_mapper: pd.DataFrame,
+    delete_file: Callable,
+    hdfs_md5sum: Callable,
+    hdfs_stat_size: Callable,
+    isdir: Callable,
+    isfile: Callable,
+    read_header: Callable,
+    write_string_to_file: Callable
 ):
     """Run the outputs module.
 
@@ -25,9 +33,24 @@ def run_output(
         write_csv (Callable): Function to write to a csv file.
          This will be the hdfs or network version depending on settings.
         run_id (int): The current run id
-
-
+        ultfoc_mapper : pd.DataFrame
+        The ULTFOC mapper DataFrame.
+        delete_file : callable
+            The function to use for deleting files.
+        hdfs_md5sum : callable
+            The function to use for calculating the MD5 checksum of a file on HDFS.
+        hdfs_stat_size : callable
+            The function to use for getting the size of a file on HDFS.
+        isdir : callable
+            The function to use for checking if a path is a directory.
+        isfile : callable
+            The function to use for checking if a path is a file.
+        read_header : callable
+            The function to use for reading the header of a file.
+        write_string_to_file : callable
+            The function to use for writing a string to a file.
     """
+
     OutputMainLogger.info("Starting short form output...")
 
     NETWORK_OR_HDFS = config["global"]["network_or_hdfs"]
@@ -44,3 +67,18 @@ def run_output(
         filename = f"output_short_form{tdate}_v{run_id}.csv"
         write_csv(f"{output_path}/output_short_form/{filename}", short_form_df)
     OutputMainLogger.info("Finished short form output.")
+
+
+    # Creating a manifest file using the Manifest class in manifest_output.py
+    manifest = Manifest(
+        outgoing_directory,
+        pipeline_run_datetime,
+        dry_run,
+        delete_file=delete_file,
+        hdfs_md5sum=hdfs_md5sum,
+        hdfs_stat_size=hdfs_stat_size,
+        isdir=isdir,
+        isfile=isfile,
+        read_header=read_header,
+        write_string_to_file=write_string_to_file,
+    )
