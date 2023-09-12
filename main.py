4d1e624c20383d788312ede78cfd673a5c18ebc4
westwj1 <James.Westwood@ext.ons.gov.uk>
2023-09-12 09:28:07 +0100

    Fix path to platform independent

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from importlib import reload
 import time
+import os
 
 import src.pipeline as src
 
 # reload the pipeline module to implement any changes
 reload(src)
-config_path = r"src\developer_config.yaml"
+config_path = os.path.join("src", "developer_config.yaml")
 
 start = time.time()
 src.run_pipeline(start, config_path)
