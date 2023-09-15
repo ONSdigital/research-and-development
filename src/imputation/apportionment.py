d7b3c9e320dde0fe7d589683dae9c482f7f70027
Griffith <anne.griffith@ons.gov.uk>
2023-09-15 16:36:38 +0100

    test for FTE apportionment

--- src/imputation/apportionment.py
+++ src/imputation/apportionment.py
@@ -1,12 +1,18 @@
 """Implement apportionment of headcount and FTE."""
 import logging
 import pandas as pd
+import numpy as np
 
 ApportionmentLogger = logging.getLogger(__name__)
 
 
 def calc_202_totals(df: pd.DataFrame) -> pd.DataFrame:
     """Calculate totals of q202 by reference.
+
+    For each reference, the sum over all instances for q202 is 
+    calculated and entered in column qtot_202_all for each instance. 
+    Also, the sums over all instances for just the civil or just 
+    defence is entered in column qtot_202_CD.
     
     Args:
         df (pd.DataFrame): The main dataset for apportionment.
@@ -14,11 +20,74 @@ def calc_202_totals(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The main dataset with totals.
     """
-    df["202_tot_all"] = df.groupby("reference")["202"].transform(sum)
-    df["202_tot_CD"] = df.groupby(["reference", "200"])["202"].transform(sum)
-    print(df.head())
+    total_202_all = df.groupby("reference")["202"].transform(sum)
+    df["tot_202_all"] = np.where(total_202_all >0, total_202_all, np.nan)
+
+    total_202_CD = df.groupby(["reference", "200"])["202"].transform(sum)
+    df["tot_202_CD"] = np.where(total_202_CD >0, total_202_CD, np.nan)
+
     return df
 
+
+def update_column(df: pd.DataFrame, col: str) -> pd.Series:
+    """Copy item in insance 0 to all other instances in a given reference.
+
+    For long form entries, questions 405 - 412 and 501 - 508 are recorded
+    in instance 0. A series is returned representing an updated version of the 
+    column with the values from instance 0 copied to all other instances.
+
+    Args:
+        df (pd.DataFrame): The main dataset for apportionment.
+        col (str): The name of the 4xx or 5xx column being treated.
+
+    Returns:
+        pd.Series: A series with the values in instance 0 copied to
+            other instances for the same reference.
+    """
+    updated_col = df.groupby("reference")[col].transform(sum)
+    return updated_col
+
+
+def calc_fte_column(df: pd.DataFrame, round_val=4) -> pd.DataFrame:
+    """Create a new column for FTE values.
+
+    FTE is Full Time Equivalent and the values from instance 0
+    are apportioned to other instances, based on values in 
+    the 4xx columns (405 - 412)
+    8 new columns are created, 4 each for Civil and Defence.
+
+    Args:
+        df (pd.DataFrame): The main dataset for apportionment.
+    Returns:
+        pd.Dataframe: The dataset with new columns for FTE.    
+    """
+    #TODO: Generalise this function to pass a dictionary {newcol:oldcol} 
+    #TODO and whether C or D.
+    df.loc[df["200"] == "C", "emp_researcher"] = round(
+        update_column(df, "405") * df["202"] / df["tot_202_CD"],
+        round_val
+    )
+    return df
+
+
+
+def apportion_headcounts(df: pd.DataFrame) -> pd.DataFrame:
+    """Create new columns for FTE values.
+
+    The values from instance 0 are apportioned to other instances, 
+    based on values in the 5xx columns (501 - 508)
+    9 new columns are created, and Civil and Defence are treated together.
+
+    Args:
+        df (pd.DataFrame): The main dataset for apportionment.
+    Returns:
+        pd.Dataframe: The dataset with new columns headcounts
+    """
+    #TODO: just one column created so far and code not tested
+    df["headcount_res_m"] = (
+        update_column(df, "501") * df["202"] / df["tot_202_all"]
+    )
+    return df
 
 def run_apportionment(df: pd.DataFrame) -> pd.DataFrame:
     """Calculate apportionment for headcount and FTE.
--- tests/test_imputation/test_apportionment.py
+++ tests/test_imputation/test_apportionment.py
@@ -3,7 +3,7 @@ import numpy as np
 from pandas import DataFrame as pandasDF
 from pandas._testing import assert_frame_equal
 
-from src.imputation.apportionment import calc_202_totals
+from src.imputation.apportionment import calc_202_totals, calc_fte_column
 
 class TestCalc202Totals:
     """Unit tests for flag_outliers functtion."""
@@ -42,8 +42,8 @@ class TestCalc202Totals:
             "instance",
             "200",
             "202",
-            "202_tot_all",
-            "202_tot_CD",
+            "tot_202_all",
+            "tot_202_CD",
         ]
 
         data = [
@@ -54,7 +54,7 @@ class TestCalc202Totals:
             [1001, 4, "C", 33044, 38815, 33333.0],
             [1001, 5, "D", 4677, 38815, 5482.0],
             [1001, 6, None, 0, 38815, np.nan],
-            [2002, 0, None, 0, 0, np.nan],
+            [2002, 0, None, 0, np.nan, np.nan],
             [2003, 0, None, 0, 350, np.nan],
             [2003, 1, "C", 100, 350, 300.0],
             [2003, 2, "C", 200, 350, 300.0],
@@ -70,4 +70,77 @@ class TestCalc202Totals:
         expected_df = self.create_expected_df()
 
         result_df = calc_202_totals(input_df)
+        assert_frame_equal(result_df, expected_df)
+
+
+class TestCalcFteColumn:
+    """Unit tests for flag_outliers functtion."""
+
+    def create_input_df(self):
+        """Create an input dataframe for the test."""
+        input_cols = [
+            "reference",
+            "instance",
+            "200",
+            "202",
+            "405",
+            "tot_202_all",
+            "tot_202_CD",
+        ]
+
+        data = [
+            [1001, 2, "C", 59, np.nan, 38815, 33333.0],
+            [1001, 1, "C", 230, np.nan, 38815, 33333.0],
+            [1001, 0, None, 0, 138.9, 38815, np.nan],
+            [1001, 3, "D", 805, np.nan, 38815, 5482.0],
+            [1001, 4, "C", 33044, np.nan, 38815, 33333.0],
+            [1001, 5, "D", 4677, np.nan, 38815, 5482.0],
+            [1001, 6, None, 0, np.nan, 38815, np.nan],
+            [2002, 0, None, 0, 0, np.nan],
+            [2003, 0, None, 0, 0, 350, np.nan],
+            [2003, 1, "C", 100, np.nan, 350, 300.0],
+            [2003, 2, "C", 200, np.nan, 350, 300.0],
+            [2003, 3, "D", 50, np.nan, 350, 50.0],
+        ]
+
+        input_df = pandasDF(data=data, columns=input_cols)
+        return input_df
+
+    def create_expected_df(self):
+        """Create dataframe for the expected output of the test."""
+        exp_cols = [
+            "reference",
+            "instance",
+            "200",
+            "202",
+            "405",
+            "tot_202_all",
+            "tot_202_CD",
+            "emp_researcher",
+        ]
+
+        data = [
+            [1001, 2, "C", 59, np.nan, 38815, 33333.0, 0.246],
+            [1001, 1, "C", 230, np.nan, 38815, 33333.0, 0.958],
+            [1001, 0, None, 0, 138.9, 38815, np.nan, np.nan],
+            [1001, 3, "D", 805, np.nan, 38815, 5482.0, np.nan],
+            [1001, 4, "C", 33044, np.nan, 38815, 33333.0, 137.696],
+            [1001, 5, "D", 4677, np.nan, 38815, 5482.0, np.nan],
+            [1001, 6, None, 0, np.nan, 38815, np.nan, np.nan],
+            [2002, 0, None, 0, 0, np.nan, np.nan],
+            [2003, 0, None, 0, 0, 350, np.nan, np.nan],
+            [2003, 1, "C", 100, np.nan, 350, 300.0, 0],
+            [2003, 2, "C", 200, np.nan, 350, 300.0, 0],
+            [2003, 3, "D", 50, np.nan, 350, 50.0, np.nan],
+        ]
+
+        expected_df = pandasDF(data=data, columns=exp_cols)
+        return expected_df
+
+    def test_calc_fte_column(self):
+        """Test for flag_outliers function."""
+        input_df = self.create_input_df()
+        expected_df = self.create_expected_df()
+
+        result_df = calc_fte_column(input_df, round_val=3)
         assert_frame_equal(result_df, expected_df)
