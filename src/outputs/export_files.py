e14f834af3bb7253bdceaee4c20b892ebf528a03
jwestw <james.westwood@ons.gov.uk>
2023-09-21 17:29:40 +0100

    Improvements in check files. Create run_export

--- src/outputs/export_files.py
+++ src/outputs/export_files.py
@@ -1,9 +1,10 @@
-"""This is a stand alone pipeline to selectively transfer output files from the output folder to the outgoing folder, along with their manifest file."""
+"""This is a stand alone pipeline to selectively transfer output files from
+the output folder to the outgoing folder, along with their manifest file."""
 
 
 import os
 import logging
-import datetime
+from datetime import datetime
 import toml
 
 from src.utils.helpers import Config_settings
@@ -36,7 +37,8 @@ if network_or_hdfs == "network":
     from src.utils.local_file_mods import local_isdir as isdir
     from src.utils.local_file_mods import local_read_header as read_header
     from src.utils.local_file_mods import (
-        local_write_string_to_file as write_string_to_file)
+        local_write_string_to_file as write_string_to_file,
+    )
 
 elif network_or_hdfs == "hdfs":
 
@@ -50,9 +52,8 @@ elif network_or_hdfs == "hdfs":
     from src.utils.hdfs_mods import hdfs_stat_size as hdfs_stat_size
     from src.utils.hdfs_mods import hdfs_isdir as isdir
     from src.utils.hdfs_mods import hdfs_read_header as read_header
-    from src.utils.hdfs_mods import (
-        hdfs_write_string_to_file as write_string_to_file)
-    
+    from src.utils.hdfs_mods import hdfs_write_string_to_file as write_string_to_file
+
 
 else:
     OutgoingLogger.error("The network_or_hdfs configuration is wrong")
@@ -65,13 +66,14 @@ OutgoingLogger.info(f"Using the {network_or_hdfs} file system as data source.")
 NETWORK_OR_HDFS = config["global"]["network_or_hdfs"]
 paths = config[f"{NETWORK_OR_HDFS}_paths"]
 output_path = paths["output_path"]
+short_form_output = os.path.join(output_path, "output_short_form")
 outgoing_folder = paths["outgoing_path"]
 
 # Getting correct headers to verify that df and output are the same
 s_f_schema_path = paths["short_form_schema"]
 short_form_schema = toml.load(s_f_schema_path)
 short_form_headers = short_form_schema["headers"]
-df_col_string = ",".join(short_form_headers)
+schema_columns_str = ",".join(short_form_headers)
 
 # Create a datetime object for the pipeline run - TODO: replace this with
 # the pipeline run datetime from the runlog object
@@ -80,42 +82,49 @@ pipeline_run_datetime = datetime.now()
 
 def get_file_choice(output_path):
     """Prompt user to select a files to transfer.
-    
+
     Returns:
         selction_list (list): A list of the files to transfer."""
     file_list = list_files(output_path)
-    selction_list = []
+    choices = []
     print("Select a file to transfer:")
     for i, file in enumerate(file_list):
         print(f"{i+1}. {file}")
-    file_num = "X" # initialize file_num to a non-integer value
+    file_num = "X"  # initialize file_num to a non-integer value
     while True and file_num != "Q":
         try:
-            file_num = int(input("Enter file number or press 'Q' to quit: "))
+            file_num = input("Enter file number or press 'Q' to quit: ")
+            file_num = int(file_num)
+            choices.append(file_num - 1)
             if file_num < 1 or file_num > len(file_list):
                 print("Invalid input. Please enter a valid file number.")
                 continue
         except ValueError:
             print("Invalid input. Please enter a valid file number.")
-        file_name = file_list[file_num-1]
-        selction_list.append(file_name)
+    selction_list = [file_list[num] for num in choices]
+
+    print(f"These are the file selected: \n {[file for file in selction_list]}")
+
     return selction_list
 
-def check_files_exist(file_list):
-    """Check that all the files in the file list exist using 
-        the imported isfile function."""
+
+def check_files_exist(dir, file_list):
+    """Check that all the files in the file list exist using
+    the imported isfile function."""
     for file in file_list:
-        if not isfile(file):
+        file_path = os.path.join(dir, file)
+        if not isfile(file_path):
             OutgoingLogger.error(f"File {file} does not exist.")
             raise FileNotFoundError
 
-def main():
+
+def run_export():
     """Main function to run the data export pipeline."""
     # Get list of files to transfer from user
-    file_list = get_file_choice()
+    file_list = get_file_choice(short_form_output)
 
     # Check that files exist
-    check_files_exist(file_list)
+    check_files_exist(short_form_output, file_list)
 
     # Creating a manifest object using the Manifest class in manifest_output.py
     manifest = Manifest(
@@ -131,29 +140,28 @@ def main():
         string_to_file_func=write_string_to_file,
     )
 
-
     # Add the short form output file to the manifest object
     for filename in file_list:
         manifest.add_file(
             f"{output_path}/output_short_form/{filename}",
-            column_header=df_col_string,
+            column_header=schema_columns_str,
             validate_col_name_length=True,
-            sep=","
+            sep=",",
         )
-
 
     # Write the manifest file to the outgoing directory
     manifest.write_manifest()
 
     # Copy files to outgoing folder
     copy_files(file_list)
-    
+
     # Move the manifest file to the outgoing folder
     manifest_file = search_files(output_path, "_manifest.json")
-    move_files()
+    move_files(manifest_file)
 
     # Log success message
     OutgoingLogger.info("Files transferred successfully.")
 
+
 if __name__ == "__main__":
-    main()
+    run_export()
