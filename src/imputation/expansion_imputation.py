04dd7dfe5c83e857b982c6fd0f689adf55fb7f58
jwestw <james.westwood@ons.gov.uk>
2023-10-05 17:05:13 +0100

    calculating each breakdown q

--- src/imputation/expansion_imputation.py
+++ src/imputation/expansion_imputation.py
@@ -1,14 +1,18 @@
 """_summary_
 """
 
+import logging
+
 from src.imputation.tmi_imputation import filter_by_column_content
+
+ExpansionLogger = logging.getLogger(__name__)
 
 
 def evaluate_imputed_2xx(group, cols_to_sum):
     """Evaluate the imputed 2xx as the sum of all 2xx over the sum of all 211
     multiplied by the imputed 211."""
-    sum_211 = group.loc["211"]
-    sum_2xx = group.loc[:, cols_to_sum].sum(axis=1)
+    sum_211 = group.loc["211"].sum(axis=1)  # scalar
+    sum_2xx = group.loc[:, cols_to_sum].sum(axis=1)  # scalar
 
     return (sum_2xx / sum_211) * group["211_imputed"]
 
@@ -22,19 +26,19 @@ def run_expansion(df, config):
     clear_resp_df = filter_by_column_content(df, "statusencoded", clear_statuses)
 
     # TODO: remove this temporary fix to cast Nans to False
-    clear_resp_df = clear_resp_df["211_trim"].fillna(False)
+    clear_resp_df["211_trim"].fillna(False, inplace=True)
 
     # Filter to exclude the same rows trimmed for 211_trim == True
-    trimmed_df = clear_resp_df.loc[df["211_trim"]]
+    trimmed_df = clear_resp_df.loc[clear_resp_df["211_trim"]]
 
     # Get all the 2xx cols, minus 211, imputed vals and trim marker
     breakdown_qs = config["2xx_breakdowns"]
 
     # Evaluate imputed 2xx
-    trimmed_df["imputed_2xx"] = (
-        trimmed_df.groupby("imp_class")
-        .apply(evaluate_imputed_2xx, args=breakdown_qs)
-        .reset_index(drop=True)
-    )
+    for breakdown_q in breakdown_qs:
+
+        trimmed_df[f"imputed_{breakdown_q}"] = trimmed_df.groupby("imp_class").apply(
+            (lambda x: evaluate_imputed_2xx(breakdown_q)).reset_index(drop=True)
+        )
 
     print(trimmed_df)
