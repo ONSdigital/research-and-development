eced380c9c8d2060a3d63618df572ec557d974a8
George Zorinyants <george.zorinyants@ons.gov.uk>
2023-10-13 14:36:50 +0100

    unit test outputs_helpers added

--- tests/test_outputs/test_outputs_helpers.py
+++ tests/test_outputs/test_outputs_helpers.py
@@ -1,9 +1,9 @@
+from src.outputs.outputs_helpers import create_output_df
+
 import pandas as pd
 import unittest
 from pandas._testing import assert_frame_equal
 import os
-print(os.getcwd())
-from src.outputs.outputs_helpers import create_output_df
 
 
 class TestSelectCols(unittest.TestCase):
@@ -19,19 +19,13 @@ class TestSelectCols(unittest.TestCase):
         df = pd.DataFrame(data)
         return df
 
-    def schema(self):
-        # Create sample schema
-        schema = {
-            "ref": {'Deduced_Data_Type': 'Int64', 'old_name': 'reference'},
-            "period": {'Deduced_Data_Type': 'Int64', 'old_name': 'period'},
-            "unavailable": {'Deduced_Data_Type': 'Int64', 'old_name': 'not_ready'}}
-        return schema
-
     def test_select_cols(self):
         # Call the create_output_df funtion
         df_input = self.data_frame()
-        actual_result = create_output_df(df_input)
-
+        mydir = r"D:\repos\research-and-development\tests\test_outputs"
+        myfile = "test_outputs_helpers.toml"
+        schema = os.path.join(mydir, myfile)
+        actual_result = create_output_df(df_input, schema)
         expected_result = pd.DataFrame(
             {
                 "ref": [1234],
@@ -40,3 +34,7 @@ class TestSelectCols(unittest.TestCase):
         )
 
         assert_frame_equal(actual_result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
