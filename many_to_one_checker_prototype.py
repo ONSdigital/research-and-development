ce8f10f8623ae9f0d8d5539955533cc380f7897d
George Zorinyants <george.zorinyants@ons.gov.uk>
2023-10-19 14:47:22 +0100

    Checked that two mappers are many to one

--- many_to_one_checker_prototype.py
+++ many_to_one_checker_prototype.py
@@ -6,13 +6,12 @@ many_to_one checker
 """
 import pandas as pd
 import os
-dir_in = r"R:\BERD Results System Development 2023\DAP_emulation\mappers\2023"
-file_in = "SIC_to_PG.csv"
+dir_in = r"R:\BERD Results System Development 2023\DAP_emulation\mappers"
+file_in = "sic_to_pg_alpha.csv"
 
 cols ={
-       "sic": "SIC 2007_CODE",
-       "pg_num": "2016 > Form PG",
-       "pg_alpha": "2016 > Pub PG"}
+       "sic": "sic",
+       "pg_alpha": "pg_alpha"}
 
 #%% Read, rename, de-duplicate
 infile = os.path.join(dir_in, file_in)
@@ -23,7 +22,7 @@ df = pd.read_csv(infile, usecols=usecols).rename(columns=names).drop_duplicates(
 
 #%%
 col_one = "pg_alpha"
-col_many = "pg_num"
+col_many = "sic"
 
 df_agg = df[[col_one, col_many]].drop_duplicates()
 
@@ -35,22 +34,5 @@ df_count = (df_agg
 
 outfile = os.path.join(
         dir_in, f"Counts {col_many} to {col_one}.csv")
-#df_count.to_csv(outfile, index=None)
+df_count.to_csv(outfile, index=None)
 
-#%%
-
-dftest=df_agg[[col_many, col_one]]
-myzip = zip(df_agg[col_many], df_agg[col_one])
-map_dict = dict(myzip)
-
-
-#%%
-from src.staging.pg_conversion import pg_to_pg_mapper
-
-print( "PG mapper function importd")
-
-# mydf = pg_to_pg_mapper(
-#         df, 
-#         mapper=df_agg,
-#         from_col="pg_num",
-#         to_col="pg_alpha")
