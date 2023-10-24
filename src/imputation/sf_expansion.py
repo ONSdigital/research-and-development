e190a199bb662e8c2b515264b5643847e3f8ae0f
jwestw <james.westwood@ons.gov.uk>
2023-10-24 10:38:45 +0100

    Everything in a for loop. Recombe ref list records

--- src/imputation/sf_expansion.py
+++ src/imputation/sf_expansion.py
@@ -5,6 +5,8 @@ from typing import List, Union
 import pandas as pd
 import logging
 
+from src.imputation.expansion_imputation import split_df_on_trim
+
 SFExpansionLogger = logging.getLogger(__name__)
 
 formtype_long = "0001"
@@ -21,7 +23,8 @@ def expansion_impute(
     """Calculate the expansion imputated values for short forms using long form data"""
 
     imp_class = group["imp_class"].values[0]
-    SFExpansionLogger.debug(f"Imputation class: {imp_class}")
+    SFExpansionLogger.debug(f"Imputation class: {imp_class}.")
+    SFExpansionLogger.debug(f"Master column: {master_col}.")
 
     # Make cols into str just in case coming through as ints
     bd_cols = [str(col) for col in break_down_cols]
@@ -32,7 +35,9 @@ def expansion_impute(
 
     # Sum the master column, e.g. "211" or "305" for the clear responders
     clear_statuses = ["Clear", "Clear - overridden"]
-    responder_mask = group["status"].isin(clear_statuses)
+    responder_mask = (
+        group["status"].isin(clear_statuses) or group["imp_marker"] == "TMI"
+    )
 
     # Combination masks to select correct records for summing
     long_form_responder_mask = responder_mask & long_mask
@@ -70,44 +75,38 @@ def run_sf_expansion(df: pd.DataFrame, config: dict) -> pd.DataFrame:
     # Exclude the records from the reference list
     refence_list = ["817"]
     ref_list_excluded_df = df[~df.cellnumber.isin(refence_list)]
-
-    # Groupby imputation class
-    grouped_by_impclass = ref_list_excluded_df.groupby("imp_class")
-    print(grouped_by_impclass)
-
-    # Set up a list to grab the dataframes
-    impute_df_lst = []
+    ref_list_only_df = df[df.cellnumber.isin(refence_list)]
 
     # Get master keys
-    master_keys = breakdown_dict.keys()
-    print(master_keys)
+    master_values = breakdown_dict.keys()
 
-    # for master_key in master_keys:
-    #     breakdown_qs = breakdown_dict[master_key]
+    for master_value in master_values:
+        SFExpansionLogger.debug(f"Processing exansion imputation for {master_value}")
+        # Filter to exclude the same rows trimmed for 211_trim == False
+        trimmed_df, nontrimmed_df = split_df_on_trim(
+            ref_list_excluded_df, f"{master_value}_trim"
+        )
+        SFExpansionLogger.debug(
+            f"There are {df.shape[0]} rows in the original df \n"
+            f"There are {nontrimmed_df.shape[0]} rows in the nontrimmed_df \n"
+            f"There are {trimmed_df.shape[0]} rows in the {master_value} trimmed_df"
+        )
 
-    #     imputed_df = grouped_by_impclass.apply(
-    #     expansion_impute,
-    #     master_key,
-    #     breakdown_qs,
-    #     formtype_long,
-    #     formtype_short
-    # )
-    #     impute_df_lst.append(imputed_df)
+        # Create group_by obj of the trimmed df
+        non_trim_grouped = nontrimmed_df.groupby("imp_class")
 
-    # 211 trim - split
+        # Calculate the imputation values for master question
+        expanded_df = non_trim_grouped.apply(
+            expansion_impute,
+            master_value,
+            break_down_cols=breakdown_dict["master_value"],
+        )
 
-    # concat back on trimmed records
+        # Concat the expanded df (processed from untrimmed records) back on to
+        # trimmed records
+        result_df = pd.concat([expanded_df, trimmed_df], axis=0)
 
-    # 305 trim - split
+    # Re-include those records from the reference list before returning df
+    result_df = pd.concat(result_df, ref_list_only_df)
 
-    # concat back on trimmed records
-
-    # emp_total headcount_total no trimming
-
-    # Combine imputed dataframes, dropping dupe columns
-    imputed_df = pd.concat(impute_df_lst, axis=1)
-
-    # Drop columns
-    imputed_df = imputed_df.loc[:, ~imputed_df.columns.duplicated()]
-
-    return imputed_df
+    return result_df
