3c66e41af9228a282a8f2c95a7023e5895611069
Cheshire <Jen.Cheshire@ons.gov.uk>
2023-10-27 15:41:04 +0100

    RDRP-449: add old names

--- src/outputs/long_form.py
+++ src/outputs/long_form.py
@@ -13,35 +13,6 @@ from src.outputs.outputs_helpers import create_output_df, create_period_year
 OutputMainLogger = logging.getLogger(__name__)
 
 
-def run_longform_prep(
-    df: pd.DataFrame,
-    round_val: int = 4,
-) -> pd.DataFrame:
-    """Prepare data for long form output.
-
-    Perform various steps to create new columns and modify existing
-    columns to prepare the main survey dataset for long form
-    micro data output.
-
-    Args:
-        df (pd.DataFrame): The survey dataframe being prepared for
-            long form output.
-        round_val (int): The number of decimal places for rounding.
-
-    Returns:
-        pd.DataFrame: The dataframe prepared for long form output.
-    """
-
-    # Filter for long-forms, CORA statuses and instance
-    df = df.loc[
-        (df["formtype"] == "0001")
-        & (df["form_status"].isin(["600", "800"]))
-        & (df["instance"] == 0)
-    ]
-
-    return df
-
-
 def output_long_form(
     df: pd.DataFrame,
     config: Dict[str, Any],
@@ -69,21 +40,28 @@ def output_long_form(
     paths = config[f"{NETWORK_OR_HDFS}_paths"]
     output_path = paths["output_path"]
 
+    # Map to the CORA statuses from the statusencoded column
+    df = map_o.create_cora_status_col(df, cora_mapper)
+
+    # Filter for long-forms, CORA statuses and instance
+    df = df.loc[
+        (df["formtype"] == "0001")
+        & (df["form_status"].isin(["600", "800"]))
+        & (df["instance"] == 0)
+    ]
+
     # Create a 'year' column
     df = create_period_year(df)
 
     # Join foriegn ownership column using ultfoc mapper
     df = map_o.join_fgn_ownership(df, ultfoc_mapper)
 
-    # Prepare the longform output dataframe
-    longform_output = run_longform_prep(df, round_val=4)
-
     # TODO Order using schema
 
-    # # Create long form output dataframe with required columns from schema
-    # schema_path = config["schema_paths"]["frozen_longform_schema"]
-    # schema_dict = load_schema(schema_path)
-    # longform_output = create_output_df(df, schema_dict)
+    # Create long form output dataframe with required columns from schema
+    schema_path = config["schema_paths"]["frozen_longform_schema"]
+    schema_dict = load_schema(schema_path)
+    longform_output = create_output_df(df, schema_dict)
 
     tdate = datetime.now().strftime("%Y-%m-%d")
     filename = f"Frozen_Base_Data_{tdate}_v{run_id}.csv"
