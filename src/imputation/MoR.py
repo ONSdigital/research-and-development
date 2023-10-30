3fb04a090b0332402d8db3048e3d5f2193cd0f61
Dylan Purches <Dylan.Purches@ons.gov.uk>
2023-10-30 10:05:10 +0000

    Update MoR comments and docstrings.

--- src/imputation/MoR.py
+++ src/imputation/MoR.py
@@ -10,9 +10,27 @@ bad_statuses = ["Form sent out", "Check needed"]
 
 
 def run_mor(df, backdata, target_vars):
+    """Function to implement Mean of Ratios method.
+
+    This is implemented by first carrying forward data from last year
+    for non-responders, and then calculating and applying growth rates
+    for each imputation class.
+
+    Args:
+        df (pd.DataFrame): Processed full responses DataFrame
+        backdata (pd.DataFrame): One period of backdata.
+        target_vars ([string]): List of variables to impute.
+
+    Returns:
+        pd.DataFrame: df with MoR applied
+    """
     to_impute_df, backdata = mor_preprocessing(df, backdata)
+
+    # Carry forwards method
     carried_forwards_df = carry_forwards(to_impute_df, backdata, target_vars)
-    carried_forwards_df["imp_marker"] = "carried_forwards"
+    carried_forwards_df["imp_marker"] = "CF"
+
+    # TODO Remove the `XXX_prev` columns (left in for QA)
     return apply_to_original(carried_forwards_df, df)
 
 
@@ -47,6 +65,19 @@ def mor_preprocessing(df, backdata):
 
 
 def carry_forwards(df, backdata, target_vars):
+    """Carry forwards matcing `backdata` values into `df` for
+    each column in `target_vars`.
+
+    Records are matched based on 'reference'.
+
+    Args:
+        df (pd.DataFrame): Processed full responses DataFrame.
+        backdata (_type_): One period of backdata.
+        target_vars (_type_): Variables to be imputed.
+
+    Returns:
+        pd.DataFrame: df with values carried forwards
+    """
     df = pd.merge(df, backdata, how="left", on="reference", suffixes=("", "_prev"))
     for var in target_vars:
         df[var] = df.loc[:, f"{var}_prev"]
