477f4a5fe49079e9c2c1c2ba0083a9b8e8537419
griffa1 <anne.griffith@ons.gov.uk>
2023-11-15 17:19:10 +0000

    read in and process ref list file

--- src/staging/staging_main.py
+++ src/staging/staging_main.py
@@ -513,6 +513,18 @@ def run_staging(
     val.validate_data_with_schema(itl1_detailed, "./config/itl1_detailed_schema.toml")
     StagingMainLogger.info("ITL1 detailed mapper File Loaded Successfully...")
 
+    # Loading ru_817_list mapper
+    StagingMainLogger.info("Loading ru_817_list mapper File...")
+    ref_list_817_path = paths["ref_list_817_path"]
+    check_file_exists(ref_list_817_path, raise_error=True)
+    ref_list_817 = read_csv(ref_list_817_path)
+    schema_path = "./config/reference_list_schema.toml"
+    # check_file_exists(schema_path, raise_error=True)
+    val.validate_data_with_schema(ref_list_817, schema_path)
+    StagingMainLogger.info("reference list mapper File Loaded Successfully...")
+    # update longform references that should be on the reference list
+    full_responses = val.update_ref_list(full_responses, ref_list_817)
+
     # Loading Civil or Defence detailed mapper
     StagingMainLogger.info("Loading Civil/Defence detailed mapper File...")
     civil_defence_detailed_path = paths["civil_defence_detailed_path"]
--- src/staging/validation.py
+++ src/staging/validation.py
@@ -631,3 +631,48 @@ def validate_cora_df(df: pd.DataFrame) -> pd.DataFrame:
 
     except ValueError as ve:
         raise ValueError("cora status mapper validation failed: " + str(ve))
+
+
+@exception_wrap
+def update_ref_list(full_df: pd.DataFrame, ref_list_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Update long form references that should be on the reference list.
+
+    For the first year (processing 2022 data) only, several references
+    should have been designated on the "reference list", ie, should have been
+    assigned cellnumber = 817, but were wrongly assigned a different cellnumber.
+
+    Args:
+        full_df (pd.DataFrame): The full_responses dataframe
+        ref_list_df (pd.DataFrame): The mapper containing updates for the cellnumber
+    Returns:
+        df (pd.DataFrame): with cellnumber and selectiontype cols updated.
+    """
+    ref_list_filtered = ref_list_df.loc[
+        (ref_list_df.formtype == 1) & (ref_list_df.cellnumber != 817)
+    ]
+    df = pd.merge(
+        full_df, 
+        ref_list_filtered[["reference", "cellnumber"]], 
+        how="left", 
+        on="reference", 
+        suffixes=("", "_new"), 
+        indicator=True
+    )
+    # check no items in the reference list mapper are missing from the full responses
+    missing_refs = df.loc[df["_merge"] == "right_only"]
+    if not missing_refs.empty:
+        msg = (
+        "The following references in the reference list mapper are not in the data:"
+        )
+        raise ValueError(msg + str(missing_ref.reference.unique()))
+
+    # update cellnumber and selectiontype where there is a match
+    match_cond = df["_merge"] == "both"
+    df = df.copy()
+    df.loc[match_cond, "cellnumber"] = 817
+    df.loc[match_cond, "selectiontype"] = "L"
+
+    df = df.drop(["_merge", "cellnumber_new"], axis=1)
+
+    return df
