9bcb97ae522c1a6923fcf195bc37c022e576d867
jwestw <james.westwood@ons.gov.uk>
2023-11-16 18:28:09 +0000

    Bug solved. df contains needed col

--- src/outputs/outputs_helpers.py
+++ src/outputs/outputs_helpers.py
@@ -17,21 +17,20 @@ def create_output_df(df: pd.DataFrame, output_schema: dict) -> pd.DataFrame:
     """
 
     # Create dict of current and required column names
-    colname_dict = {
+    colname_schema_dict = {
         output_schema[column_nm]["old_name"]: column_nm
         for column_nm in output_schema.keys()
     }
 
     # Create subset dataframe with only the required outputs
-    output_df = df[df.columns.intersection(colname_dict.keys())].copy()
+    schema_keys = set()
+    output_df = df[colname_schema_dict.keys()].copy()
 
     # Rename columns to match the output specification
-    output_df.rename(
-        columns={key: colname_dict[key] for key in colname_dict}, inplace=True
-    )
+    output_df.rename(columns=colname_schema_dict, inplace=True)
 
     # Rearrange to match user defined output order
-    output_df = output_df[colname_dict.values()]
+    output_df = output_df[colname_schema_dict.values()]
 
     return output_df
 
