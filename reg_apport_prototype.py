adda0e861a4d83af4919b5d86079c289bd5f4be9
George Zorinyants <george.zorinyants@ons.gov.uk>
2023-12-05 17:00:24 +0000

    Functionalised the short forms

--- reg_apport_prototype.py
+++ reg_apport_prototype.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
+#%% -*- coding: utf-8 -*-
 """
 Created on Wed Nov 22 11:46:52 2023
-harmonise lengths
 @author: zoring
 
 """
@@ -18,17 +17,16 @@ ins_col = "instance"
 period_col = "period"
 short_code = "0006"
 long_code = "0001"
-
+form_col = "formtype"
+percent_col = "602"
 short_percent = 100.0
 value_cols = ["407", "408", "409"]
 
 # %%  Functions
-def short_percent(df):
-    form_col = "formtype"
-    pecent_col = "602"
-    short_percent = 100.0
-    cond = df[form_col] == short_code
-    return df[pecent_col].mask(cond, other=short_percent, inplace=True)
+def run_short(df):
+    cond = (df[form_col] == short_code)
+    df[percent_col].mask(cond, other=short_percent, inplace=True)
+    return df
 
 
 #%%
@@ -40,20 +38,22 @@ print(f"Input df is read. Columns are:\n{df.dtypes}")
 cases = {}
 
 #%% Case A Assign 100 to short forms
-cases.update({"short form": short_percent})
+cases.update({"short form": run_short})
 
-#df_out = short_percent(df)
+#%%
+for key in cases:
+    df_out = cases[key](df)
 
 #%%
 
-cond = cond = df[form_col] == long_code
+# cond = cond = df[form_col] == long_code
 
-df_l = df.copy()[cond]
-#%%
-dfg = df_l.groupby([ref_col, period_col])
+# df_l = df.copy()[cond]
+# #%%
+# dfg = df_l.groupby([ref_col, period_col])
 
-#%%
-mypath = os.path.join(mydir, out_file)
-#df_out.to_csv(mypath, index=None)
-print(f"Output is saved")
+# #%%
+# mypath = os.path.join(mydir, out_file)
+# #df_out.to_csv(mypath, index=None)
+# print(f"Output is saved")
 
