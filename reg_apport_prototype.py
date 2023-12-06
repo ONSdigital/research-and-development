183906fb043db9795a9d3118e9f9794c4e1eac72
George Zorinyants <george.zorinyants@ons.gov.uk>
2023-12-06 15:21:42 +0000

    Slight re-arrangement

--- reg_apport_prototype.py
+++ reg_apport_prototype.py
@@ -26,7 +26,7 @@ cols = {
     "civdef": "200",
 }
 
-want_cals = [str(x) for x in range(202, 251)] + [str(x) for x in range(300, 509)] 
+want_cals = [str(x) for x in range(202, 509)] 
 
 short_code = "0006"
 long_code = "0001"
@@ -34,38 +34,18 @@ short_percent = 100.0
 
 
 # %%  Functions
-def run_short(df):
-    cond = (df[cols["form"]] == short_code)
-    df[cols["percent"]].mask(cond, other=short_percent, inplace=True)
-    return df
 
+# Cleaning the short forms
+def run_short(df: pd.DataFrame) -> pd.DataFrame:
+    df_out = df.copy()
+    cond = (df_out[cols["form"]] == short_code)
+    df_out[cols["percent"]].mask(cond, other=short_percent, inplace=True)
+    return df_out
 
-#%%
-mypath = os.path.join(mydir, in_file)
-df = pd.read_pickle(mypath)
-print(f"Input df is read. Columns are:\n{df.dtypes}")
-
-#%% Cases
-cases = {}
-
-#%% Case A Assign 100 to short forms
-cases.update({"short form": run_short})
-
-#%%
-for key in cases:
-    df_out = cases[key](df)
-
-#%% Extract the long forms
-
-cond = df[cols["form"]] == long_code
-
-df = df[cond]
-
-#%% Extract the forms that have multiple postcodes and single PG
-def count_codes(df: pd.DataFrame, code: str = "postcode"):
+# Counting unique non-blank codes
+def count_codes(df: pd.DataFrame, code: str = "postcode") -> pd.DataFrame:
     # Calculates the number of unique non-empty codes
 
-    # Calculate the number of unique non-zero postcodes 
     dfa = df.copy()
     # Select columns that we need
     cols_need = [cols["ref"], cols["period"], cols[code]]
@@ -79,36 +59,9 @@ def count_codes(df: pd.DataFrame, code: str = "postcode"):
         on = [cols["ref"], cols["period"]],
         how="left")
     return df
-#%%
-df = count_codes(df, "postcode")
-df = count_codes(df, "product")
-df = count_codes(df, "civdef")
 
-#%% Compute weights
-dfc = df.copy()
-dfc = dfc[
-    (dfc[cols["postcode"] + "_count"] > 1) &
-    (dfc[cols["product"] + "_count"] == 1)
-]
-
-#% Create a separate column for site weights
-dfc["s_percent"] = dfc[cols["percent"]]
-dfc["s_percent"].fillna(0, inplace=True)
-#%% Set the weight for instance 0 to be 0
-dfc["s_percent"] = dfc["s_percent"] * dfc[cols["ins"]].astype("bool")
-
-#%% Calculate the total per ref per period
-dfc["s_percent_total"] = (
-    dfc.groupby([cols["ref"], cols["period"]])["s_percent"].transform("sum"))
-
-#%% Filter out the rows where total percent is zero
-
-dfc= dfc[dfc["s_percent_total"] != 0]
-
-#%% Compute weights
-dfc["s_weight"] = dfc["s_percent"] / dfc["s_percent_total"]
-
-#%% Apply weights - create new columns
+# Computes the total numerical value across multiple sites and spreads it using 
+# the weights
 def value_to_sites(dfc : pd.DataFrame, vc: str) -> pd.DataFrame:
     vcs = vc + "_s"
     dfc[vcs] = dfc[vc].fillna(0)
@@ -117,16 +70,59 @@ def value_to_sites(dfc : pd.DataFrame, vc: str) -> pd.DataFrame:
     dfc[vcs].replace(0, np.nan, inplace=True)
     return dfc
 
-#%% Calculate which columns are numeric
+#%% Load input data
+mypath = os.path.join(mydir, in_file)
+df = pd.read_pickle(mypath)
+print(f"Input df is read. Columns are:\n{df.dtypes}")
+
+#%% Main functio
+# Assign 100 to the "percent" column of short forms 
+df_out = run_short(df)
+
+#%% Apportionment of long forms 
+# Extract the long forms
+cond = df[cols["form"]] == long_code
+df = df[cond]
+
+# Count distinct non-empty codes
+df = count_codes(df, "postcode")
+df = count_codes(df, "product")
+df = count_codes(df, "civdef")
+
+# Selecting cases with one product, many sites
+dfc = df.copy()
+dfc = dfc[
+    (dfc[cols["postcode"] + "_count"] > 1) &
+    (dfc[cols["product"] + "_count"] == 1)
+]
+
+# Create a separate column for site percents
+dfc["s_percent"] = dfc[cols["percent"]]
+dfc["s_percent"].fillna(0, inplace=True)
+
+# Set the weight for instance 0 to be 0
+dfc["s_percent"] = dfc["s_percent"] * dfc[cols["ins"]].astype("bool")
+
+#%% Calculate the total percent for each reference and period
+dfc["s_percent_total"] = (
+    dfc.groupby([cols["ref"], cols["period"]])["s_percent"].transform("sum"))
+
+# Filter out the rows where total percent is zero
+dfc= dfc[dfc["s_percent_total"] != 0]
+
+# Compute weights
+dfc["s_weight"] = dfc["s_percent"] / dfc["s_percent_total"]
+
+# Applying weights
+# Calculate which value columns are in the data and are numeric
 exist_cols = [x for x in want_cals if x in dfc.columns]
 value_cols = [x for x in exist_cols if is_numeric_dtype(dfc[x])]
 
-
-#%% Apportion columns to sites
+# Calculates the apportioned value for all value columns
 for vc in value_cols:
     dfc = value_to_sites(dfc, vc)
 
-#%% Repeat the product group
+# Repeat the product group and C or D marker across multiple sites
 key_cols = ["product", "civdef"]
 for col in key_cols:
     dfc[cols[col]].fillna("", inplace=True)
@@ -136,25 +132,25 @@ for col in key_cols:
         .groupby([cols["ref"], cols["period"]])[cols[col]]
         .transform("max"))
 
-
-#%% Replace initial values with apportioned values
+# Chooses the columns to merge back to the original data
 indexcols = [cols["ref"], cols["period"], cols["ins"]]
 svaluecols = [x + "_s" for x in value_cols]
-usecols = indexcols + svaluecols + [cols[col] + "_s" for col in key_cols] 
+scodecols = [cols[col] + "_s" for col in key_cols]
+usecols = indexcols + svaluecols + scodecols
 dfc = dfc[usecols]
-# %%
+
+# Merges the apportioned values and repeated code back to the main dataframe
 df_out = df_out.merge(dfc, on=indexcols, how="left")
 
-
-#%% Replace the values and remove the columns _s
+# Replace the values and remove the columns _s
 key_names = [cols[x] for x in cols if x in key_cols]
 for vc in value_cols + key_names:
     _ = df_out.loc[~df_out[vc + "_s"].isnull(), vc] = df_out[vc + "_s"]
-df_out.drop(columns=svaluecols, inplace=True)
-#%%
-# for col in key_cols:
-#     df_out.loc[~df_out[cols[col] + "_s"].isnull(), cols[col]] = df_out[cols[col] + "_s"]
-#%% Save
+
+# Removes the columns with underscore _s
+df_out.drop(columns=(svaluecols + scodecols), inplace=True)
+
+# Save the output
 mypath = os.path.join(mydir, out_file)
 df_out.to_csv(mypath, index=None)
 print(f"Output is saved")
