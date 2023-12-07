0e8658901d61b230d79b67dabccee10c824ec216
Cheshire <Jen.Cheshire@ons.gov.uk>
2023-12-07 11:46:02 +0000

    RDRP-555: remove topup code from outputs

--- src/outputs/outputs_main.py
+++ src/outputs/outputs_main.py
@@ -82,15 +82,6 @@ def run_outputs(
         )
         OutputMainLogger.info("Finished status filtered output.")
 
-    # outputs_df = outputs_df.astype({"postcodes_harmonised": "str"})
-    # outputs_df["postcodes_harmonised"] = outputs_df["postcodes_harmonised"].apply(
-    #     postcode_topup
-    # )
-
-    # tau_outputs_df = tau_outputs_df.astype({"postcodes_harmonised": "str"})
-    # tau_outputs_df["postcodes_harmonised"] = (
-    #     tau_outputs_df["postcodes_harmonised"].apply(postcode_topup))
-
     # Running short form output
     if config["global"]["output_short_form"]:
         OutputMainLogger.info("Starting short form output...")
