003c24620f318fa7e969b04de627709ef4755de7
Tom Coates <tom.coates@ons.gov.uk>
2023-12-13 12:43:31 +0000

    add type hinting to NI data

--- src/northern_ireland/ni_main.py
+++ src/northern_ireland/ni_main.py
@@ -1,12 +1,20 @@
 """The main file for the Northern Ireland  module."""
 
 import logging
+import pandas as pd
+from typing import Callable
 from src.northern_ireland.ni_staging import run_ni_staging
 from src.construction.construction import run_construction
 
 NIModuleLogger = logging.getLogger(__name__)
 
-def run_ni(config, check_file_exists, read_csv, write_csv, run_id):
+def run_ni(
+    config: dict,
+    check_file_exists: Callable,
+    read_csv: Callable,
+    write_csv: Callable,
+    run_id: int,
+) -> pd.DataFrame:
     """Run the Northern Ireland module to stage and apply construction.
 
     Args:
@@ -23,7 +31,7 @@ def run_ni(config, check_file_exists, read_csv, write_csv, run_id):
             data with any constructed records amended.
     """
     load_ni_data = config["global"]["load_ni_data"]
-    if load_ni_data is False:
+    if not load_ni_data:
         NIModuleLogger.info("Skipping Northern Ireland data...")
         return None
     
