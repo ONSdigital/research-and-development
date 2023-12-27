3f9b3ff9e05080e789054975d5f8f880cea5fa90
jwestw <james.westwood@ons.gov.uk>
2023-12-27 13:22:16 +0000

    Debugger statements

--- src/outputs/gb_sas.py
+++ src/outputs/gb_sas.py
@@ -9,7 +9,7 @@ from src.staging.validation import load_schema
 from src.outputs.outputs_helpers import create_output_df, regions
 from src.staging.pg_conversion import sic_to_pg_mapper
 
-OutputMainLogger = logging.getLogger(__name__)
+GbSasLogger = logging.getLogger(__name__)
 
 
 def output_gb_sas(
@@ -41,16 +41,25 @@ def output_gb_sas(
     paths = config[f"{NETWORK_OR_HDFS}_paths"]
     output_path = paths["output_path"]
 
+    GbSasLogger.debug(f"1: df shape: {df.shape}")
+    GbSasLogger.debug(f"1: 211 total: {df['211'].sum()}")
     # Filter out records that answer "no R&D"
-    df = df.copy().loc[~((df["604"] == "No") & (df["211"] > 0) & (~df["211"].isna()))]
+    df = df.copy().loc[~((df["604"] == "No") & (df["211"] > 0))]
+    GbSasLogger.debug(f"2: df shape: {df.shape}")
+    GbSasLogger.debug(f"2: 211 total: {df['211'].sum()}")
 
     # Filter regions for GB only
     df = df.copy().loc[df["region"].isin(regions()["GB"])]
+    GbSasLogger.debug(f"3: df shape: {df.shape}")
+    GbSasLogger.debug(f"3: 211 total: {df['211'].sum()}")
 
     # Prepare the columns needed for outputs:
 
+    GbSasLogger.debug(f"4: df shape: {df.shape}")
     # Join foriegn ownership column using ultfoc mapper
     df = map_o.join_fgn_ownership(df, ultfoc_mapper)
+    GbSasLogger.debug(f"5: df shape: {df.shape}")
+    GbSasLogger.debug(f"5: 211 total: {df['211'].sum()}")
 
     # Fill in numeric PG for short forms and imputed long forms
     df = sic_to_pg_mapper(
@@ -62,30 +71,47 @@ def output_gb_sas(
         formtype=["0006", "0001"],
     )
 
+    GbSasLogger.debug(f"6: df shape: {df.shape}")
+    GbSasLogger.debug(f"6: 211 total: {df['211'].sum()}")
+
     # Map to the CORA statuses from the statusencoded column
     df = map_o.create_cora_status_col(df, cora_mapper)
+    GbSasLogger.debug(f"7: df shape: {df.shape}")
+    GbSasLogger.debug(f"7: 211 total: {df['211'].sum()}")
 
     # Map the sizebands based on frozen employment
     df = map_o.map_sizebands(df)
+    GbSasLogger.debug(f"8: df shape: {df.shape}")
+    GbSasLogger.debug(f"8: 211 total: {df['211'].sum()}")
 
     # Map the itl regions using the postcodes
     df = map_o.join_itl_regions(df, postcode_mapper)
+    GbSasLogger.debug(f"9: df shape: {df.shape}")
+    GbSasLogger.debug(f"9: 211 total: {df['211'].sum()}")
 
     # Create C_lnd_bl
     df["C_lnd_bl"] = df["219"] + df["220"]
+    GbSasLogger.debug(f"10: df shape: {df.shape}")
+    GbSasLogger.debug(f"10: 211 total: {df['211'].sum()}")
 
     # Create ovss_oth
     df["ovss_oth"] = (
         df["243"] + df["244"] + df["245"] + df["246"] + df["247"] + df["249"]
     )
+    GbSasLogger.debug(f"11: df shape: {df.shape}")
+    GbSasLogger.debug(f"11: 211 total: {df['211'].sum()}")
 
     # Create oth_sc
     df["oth_sc"] = df["242"] + df["248"] + df["250"]
+    GbSasLogger.debug(f"12: df shape: {df.shape}")
+    GbSasLogger.debug(f"12: 211 total: {df['211'].sum()}")
 
     # Create GB SAS output dataframe with required columns from schema
     schema_path = config["schema_paths"]["gb_sas_schema"]
     schema_dict = load_schema(schema_path)
     output = create_output_df(df, schema_dict)
+    GbSasLogger.debug(f"13: df shape: {df.shape}")
+    GbSasLogger.debug(f"13: 211 total: {df['211'].sum()}")
 
     # Outputting the CSV file with timestamp and run_id
     tdate = datetime.now().strftime("%Y-%m-%d")
