6a8ba8abb122585fe3536684c1e29fb369624fd8
jwestw <james.westwood@ons.gov.uk>
2023-12-28 15:44:36 +0000

    Clearning commented out code

--- src/staging/staging_main.py
+++ src/staging/staging_main.py
@@ -433,17 +433,6 @@ def run_staging(
         backdata = None
         StagingMainLogger.info("Loading of Backdata File skipped")
 
-    # # Load cora mapper
-    # StagingMainLogger.info("Loading Cora status mapper file")
-    # cora_mapper_path = paths["cora_mapper_path"]
-    # check_file_exists(cora_mapper_path, raise_error=True)
-    # cora_mapper = read_csv(cora_mapper_path)
-    # # validates and updates from int64 to string type
-    # val.validate_data_with_schema(cora_mapper, "./config/cora_schema.toml")
-
-    # cora_mapper = val.validate_cora_df(cora_mapper)
-    # StagingMainLogger.info("Cora status mapper file loaded successfully...")
-
     # Load ultfoc (Foreign Ownership) mapper
     StagingMainLogger.info("Loading Foreign Ownership File")
     ultfoc_mapper_path = paths["ultfoc_mapper_path"]
