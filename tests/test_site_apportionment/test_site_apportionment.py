978360297d1af454c380863359e2f1873f94d74a
Griffith <anne.griffith@ons.gov.uk>
2024-02-14 16:13:04 +0000

    706 test for create_notnull_mask

--- tests/test_site_apportionment/test_site_apportionment.py
+++ tests/test_site_apportionment/test_site_apportionment.py
@@ -7,10 +7,109 @@ from src.site_apportionment.site_apportionment import create_notnull_mask
 
 
 class TestCreateNotnullMask():
-
+    """Tests for the function create_not_null_mask."""
     def create_input_df(self):
         """Create an input dataframe for the test."""
-        input_columns = [
+        input_columns = ["reference", "col"]
+            
+        data = [
+            [0, 1],
+            [1, "a"],
+            [2, ""],
+            [3, pd.NA],
+            [4, np.nan],
+            [5, " "],
+        ]   
+
+        input_df = pandasDF(data=data, columns=input_columns)
+        return input_df 
+
+
+    def test_create_notnull_mask(self):
+        """Test for create_notnull_mask function."""
+        self.input_df = self.create_input_df()
+
+        exp_output = pd.Series([False, True, False, False, False, True], name="col")
+
+        result = create_notnull_mask(self.input_df, "col")
+        assert_series_equal(result, exp_output)
+
+
+    def create_demo_expected_df(self):
+        """Create an input dataframe to show how other nulls work."""
+        expected_columns = [
+            "reference",
+            "col",
+            "this_test",
+            "flag_is_null",
+            "flag_is_emty",
+            "flag_is_notnull",
+            "flag_not_is_null",
+            "len",
+            "flag_length_greater_than_zero",
+        ]
+            
+        data = [
+            [0, 1, False, np.nan, np.nan, True, True, np.nan, np.nan],
+            [1, "a", True, np.nan, np.nan, True, True, 1.0, True],
+            [2, "", False, np.nan, True, True, True, 0.0, np.nan],
+            [3, pd.NA, False, True, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [4, np.nan, False, True, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [5, " ", True, np.nan, np.nan, True, True, 1.0, True],    
+        ]   
+
+        expected_df = pandasDF(data=data, columns=expected_columns)
+        return expected_df  
+
+    def test_other_nulls_demo(self):
+        """Demonstrate how this function works and compare with other tests for null.
+        
+        The column under test has the following values:
+        [1, "a", "", pd.NA, np.nan, " "]
+        """
+        demo_df = self.create_input_df()
+
+        # here's the original test again
+        result = create_notnull_mask(demo_df, "col")
+        exp_output = pd.Series([False, True, False, False, False, True], name="col")
+        assert_series_equal(result, exp_output)
+
+        # test the effect of apply applying .isnull() to column col
+        result_not_isnull = ~demo_df["col"].isnull()
+        # note that "" does not register as null
+        exp_isnull = pd.Series([True, True, True, False, False, True], name="col")
+        assert_series_equal(result_not_isnull, exp_isnull)
+ 
+        # test the effect of apply applying .notnull() to column col
+        result_notnull = demo_df["col"].notnull()
+        # note that "" registers as not null
+        exp_notnull = pd.Series([True, True, True, False, False, True], name="col")
+        assert_series_equal(result_notnull, exp_notnull)
+
+        # test applying str.len() to column col
+        result_len = demo_df["col"].str.len()
+        exp_len = pd.Series([np.nan, 1.0, 0.0, np.nan, np.nan, 1.0], name="col")
+        assert_series_equal(result_len, exp_len)
+
+    def test_other_nulls_dataframe_demo(self):
+        """Dataframe visualisation of tests for nulls.
+        
+        The column under test has the following values:
+        [1, "a", "", pd.NA, np.nan, " "]
+        """
+        # set up an input dataframe 
+        demo_df = self.create_input_df()
+
+        # create new mask columns in demo_df for various null-checking functions
+        demo_df.loc[demo_df["col"].isnull(), "flag_is_null"] = True
+        demo_df.loc[demo_df["col"] == "", "flag_is_emty"] = True
+        demo_df.loc[demo_df["col"].notnull(), "flag_is_notnull"] = True
+        demo_df.loc[~demo_df["col"].isnull(), "flag_not_is_null"] = True
+        demo_df["len"] = demo_df["col"].str.len()
+        demo_df.loc[demo_df["len"] > 0, "flag_length_greater_than_zero"] = True
+
+        # create the expected dataframe for these operations
+        expected_columns = [
             "reference",
             "col",
             "flag_is_null",
@@ -22,39 +121,15 @@ class TestCreateNotnullMask():
         ]
             
         data = [
-            [0, 1, np.nan, np.nan, True, True, np.nan, np.nan],
-            [1, "a", np.nan, np.nan, True, True, 1.0, True],
-            [2, "", np.nan, True, True, True, 0.0, np.nan],
-            [3, pd.NA, True, np.nan, np.nan, np.nan, np.nan, np.nan],
-            [4, np.nan, True, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [0, 1,      np.nan, np.nan, True,   True,   np.nan, np.nan],
+            [1, "a",    np.nan, np.nan, True,   True,   1.0,    True],
+            [2, "",     np.nan, True,   True,   True,   0.0,    np.nan],
+            [3, pd.NA,  True,   np.nan, np.nan, np.nan, np.nan, np.nan],
+            [4, np.nan, True,   np.nan, np.nan, np.nan, np.nan, np.nan],
+            [5, " ",    np.nan, np.nan, True,   True,   1.0,    True],
         ]   
 
-        input_df = pandasDF(data=data, columns=input_columns)
-        return input_df  
+        expected_df = pandasDF(data=data, columns=expected_columns)
 
-    def create_exp_output(self):
-        """Create a series for the expected output.""" 
-        name = "col"
-        data = [False, True, False, False, False] 
-        return pd.Series(data, name=name)
-    
-    def test_create_notnull_mask(self):
-        """Test for create_notnull_mask function."""
-        df=pd.DataFrame(
-            {"reference": [0, 1, 2, 3, 4],
-            "col":[1, "a", "", pd.NA, np.nan]})
-
-        # add extra cols to the df to illustrate how logic of nulls
-        df.loc[df["col"].isnull(), "flag_is_null"] = True
-        df.loc[df["col"] == "", "flag_is_emty"] = True
-        df.loc[df["col"].notnull(), "flag_is_notnull"] = True
-        df.loc[~df["col"].isnull(), "flag_not_is_null"] = True
-        df["len"] = df["col"].str.len()
-        df.loc[df["len"] > 0, "flag_length_greater_than_zero"] = True
-
-        input_df = self.create_input_df()
-        assert_frame_equal(df, input_df)
-
-        result = create_notnull_mask(input_df, "col")
-        expected_output = self.create_exp_output()
-        assert_series_equal(result, expected_output)
+        # test that the operations in this function give the expected output
+        assert_frame_equal(demo_df, expected_df)
