016fbe628bf5ba451faec8a25c8abd5ad1f97d59
Griffith <anne.griffith@ons.gov.uk>
2024-02-20 10:47:35 +0000

    706 unit test for split_sites_df

--- src/site_apportionment/site_apportionment.py
+++ src/site_apportionment/site_apportionment.py
@@ -58,14 +58,16 @@ def set_percentages(df: pd.DataFrame) -> pd.DataFrame:
         raise ValueError("Percent column for short forms should be blank.")
     df.loc[df[form_col] == short_code, percent_col] = 100
 
-    
-    # Condition for long forms with status = "Form sent out"
+    # Condition for long forms with status "Form sent out"
+    # Note: those imputed by MoR will have had the postcode column imputed, so we check
+    # for null in the postcode count column
     sent_out_condition = (
         (df[form_col] == long_code)
         & (df[status_col] == "Form sent out")
         & (df[postcode_col + "_count"].isna())
         & (df[postcode_col].isna())
     )
+    # Update records matching the sent_out_condition with a postcode and count of 1
     df.loc[sent_out_condition, postcode_col] = (
         df.loc[sent_out_condition, "postcodes_harmonised"])
     df.loc[sent_out_condition, postcode_col + "_count"] = 1
@@ -111,14 +113,18 @@ def split_sites_df(
     """
     Split dataframe into two based on whether there are sites or not.
 
+    This will be the basis on whether or not records are included in site apportionment.
+
     All long form records that include postcodes in the postcode_col are used for site 
-    apportionment, and all orther records are included in a second dataframe.
+    apportionment, and all orther records, including short forms, are included in a 
+    second dataframe.
 
     Args:
         df (pd.DataFrame): The input DataFrame.
 
     Returns:
-        Tuple[pd.DataFrame, pd.DataFrame]: A tuple containing two DataFrames.
+        Tuple[pd.DataFrame, pd.DataFrame]: One dataframe with records to be apportioned,
+            and a second containing all other records.
     """
 
     # Condition for records to apportion: long forms, at least one site, instance >=1
@@ -135,6 +141,7 @@ def split_sites_df(
     df_out = df.copy()[~to_apportion_cond]
 
     # Remove "bad" imputation markers from df_out
+    # NOTE: Probably this isn't needed: can't think of a case where it would be.
     df_out = keep_good_markers(df_out, imp_markers_to_keep)
 
     return to_apportion_df, df_out
@@ -216,7 +223,7 @@ def create_category_df(
     )
     category_df = category_df.loc[valid_code_cond]
 
-    # Remove "bad" imputation markers, "no imputation" and "no mean found."
+    # Remove "bad" imputation markers, "no_imputation" and "No mean found."
     category_df = keep_good_markers(category_df, imp_markers_to_keep)
 
     # De-duplicate - possibly, not needed
--- tests/test_site_apportionment/test_site_apportionment.py
+++ tests/test_site_apportionment/test_site_apportionment.py
@@ -1,13 +1,50 @@
 import pandas as pd
 import numpy as np
+import pytest
 from pandas import DataFrame as pandasDF
 from pandas._testing import assert_frame_equal, assert_series_equal
 
 from src.site_apportionment.site_apportionment import (
     create_notnull_mask,
     set_percentages,
-    count_unique_postcodes_in_col,
+    split_sites_df,
 )
+
+@pytest.fixture
+def create_exp_percent_test_output_df():
+    """Create a dataframe for expected output of the for the set_percentages function test.
+    
+    NOTE: this dataframe will also be the input for the split_sites function test.
+    """
+    exp_output_columns = [
+        "reference",
+        "instance",
+        "formtype",
+        "601",
+        "602",
+        "601_count",
+        "status",
+        "imp_marker",
+        "postcodes_harmonised",
+    ]
+        
+    data = [
+        [1, 0, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
+        [1, 1, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
+        [1, 2, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
+        [2, 0, "0001", np.nan, np.nan, 2.0, "Clear", "R", "NP20 6YY"],
+        [2, 1, "0001", "CB1 3NF", 60.0, 2.0, "Clear", "R", "CB1 3NF"],
+        [2, 2, "0001", "BA1 5DA", 40.0, 2.0, "Clear", "R", "BA1 5DA"],
+        [3, 0, "0001", np.nan, np.nan, 1.0, "Check needed", "TMI", "NP30 7ZZ"],
+        [3, 1, "0001", "DE72 3AU", 100.0, 1.0, "Check needed", "TMI", "DE72 3AU"],
+        [3, 2, "0001", np.nan, np.nan, 1.0, "Check needed", "No mean found", "NP30 7ZZ"],
+        [4, 1, "0001", "CF10 BZZ", 100.0, 1.0, "Form sent out", "TMI", "CF10 BZZ"],
+        [5, 1, "0001", "SA50 5BE", 100.0, 1.0, "Form sent out", "No mean found", "SA50 5BE"],
+    ]   
+
+    exp_output_df = pandasDF(data=data, columns=exp_output_columns)
+    return exp_output_df
+    
 
 class TestSetPercentages():
     """Tests for the set_percentages function."""
@@ -33,9 +70,9 @@ class TestSetPercentages():
             [2, 0, "0001", np.nan, np.nan, 2.0, "Clear", "R", "NP20 6YY"],
             [2, 1, "0001", "CB1 3NF", 60.0, 2.0, "Clear", "R", "CB1 3NF"],
             [2, 2, "0001", "BA1 5DA", 40.0, 2.0, "Clear", "R", "BA1 5DA"],
-            [3, 0, "0001", np.nan, np.nan, 1.0, "Clear", "R", "NP30 7ZZ"],
-            [3, 1, "0001", "DE72 3AU", np.nan, 1.0, "Clear", "R", "DE72 3AU"],
-            [3, 2, "0001", np.nan, np.nan, 1.0, "Clear", "R", "NP30 7ZZ"],
+            [3, 0, "0001", np.nan, np.nan, 1.0, "Check needed", "TMI", "NP30 7ZZ"],
+            [3, 1, "0001", "DE72 3AU", np.nan, 1.0, "Check needed", "TMI", "DE72 3AU"],
+            [3, 2, "0001", np.nan, np.nan, 1.0, "Check needed", "No mean found", "NP30 7ZZ"],
             [4, 1, "0001", np.nan, np.nan, np.nan, "Form sent out", "TMI", "CF10 BZZ"],
             [5, 1, "0001", np.nan, np.nan, np.nan, "Form sent out", "No mean found", "SA50 5BE"],
         ]   
@@ -43,9 +80,20 @@ class TestSetPercentages():
         input_df = pandasDF(data=data, columns=input_columns)
         return input_df
     
-    def create_exp_output_df(self):
-        """Create an input dataframe for the test."""
-        exp_output_columns = [
+    def test_set_percentage(self, create_exp_percent_test_output_df):
+        input_df = self.create_input_df()
+        expected_output_df = create_exp_percent_test_output_df
+
+        result_df = set_percentages(input_df)
+        assert_frame_equal(result_df, expected_output_df)
+
+
+class TestSplitSitesDf():
+    """Tests for the split_sites_df function."""
+
+    def create_exp_to_apportion_output(self):
+        """Expected output for the to_apportion_df dataframe."""
+        exp_output_cols1 = [
             "reference",
             "instance",
             "formtype",
@@ -57,30 +105,63 @@ class TestSetPercentages():
             "postcodes_harmonised",
         ]
             
-        data = [
+        data1 = [
+            [2, 1, "0001", "CB1 3NF", 60.0, 2.0, "Clear", "R", "CB1 3NF"],
+            [2, 2, "0001", "BA1 5DA", 40.0, 2.0, "Clear", "R", "BA1 5DA"],
+            [3, 1, "0001", "DE72 3AU", 100.0, 1.0, "Check needed", "TMI", "DE72 3AU"],
+            [3, 2, "0001", np.nan, np.nan, 1.0, "Check needed", "No mean found", "NP30 7ZZ"],
+            [4, 1, "0001", "CF10 BZZ", 100.0, 1.0, "Form sent out", "TMI", "CF10 BZZ"],
+            [5, 1, "0001", "SA50 5BE", 100.0, 1.0, "Form sent out", "No mean found", "SA50 5BE"],
+    ]   
+
+        exp_output_df1 = pandasDF(data=data1, columns=exp_output_cols1)
+        return exp_output_df1
+    
+    def create_exp_remaining_output(self):
+        """Expected output for the df_out dataframe.
+        
+        This should contain all the records not in the to_apportion dataframe,
+        except those with imp_marker = "No mean found" or "no_imputation"
+        """
+        exp_output_cols2 = [
+            "reference",
+            "instance",
+            "formtype",
+            "601",
+            "602",
+            "601_count",
+            "status",
+            "imp_marker",
+            "postcodes_harmonised",
+        ]
+            
+        data2 = [
             [1, 0, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
             [1, 1, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
             [1, 2, "0006", np.nan, 100.0, np.nan, "Clear", "R", "NP10 5XX"],
             [2, 0, "0001", np.nan, np.nan, 2.0, "Clear", "R", "NP20 6YY"],
-            [2, 1, "0001", "CB1 3NF", 60.0, 2.0, "Clear", "R", "CB1 3NF"],
-            [2, 2, "0001", "BA1 5DA", 40.0, 2.0, "Clear", "R", "BA1 5DA"],
-            [3, 0, "0001", np.nan, np.nan, 1.0, "Clear", "R", "NP30 7ZZ"],
-            [3, 1, "0001", "DE72 3AU", 100.0, 1.0, "Clear", "R", "DE72 3AU"],
-            [3, 2, "0001", np.nan, np.nan, 1.0, "Clear", "R", "NP30 7ZZ"],
-            [4, 1, "0001", "CF10 BZZ", 100.0, 1.0, "Form sent out", "TMI", "CF10 BZZ"],
-            [5, 1, "0001", "SA50 5BE", 100.0, 1.0, "Form sent out", "No mean found", "SA50 5BE"],
+            [3, 0, "0001", np.nan, np.nan, 1.0, "Check needed", "TMI", "NP30 7ZZ"],
         ]   
-
-        exp_output_df = pandasDF(data=data, columns=exp_output_columns)
-        return exp_output_df
+        exp_output_df2 = pandasDF(data=data2, columns=exp_output_cols2)
+        exp_output_df2 = exp_output_df2.astype({"601": object})
+        return exp_output_df2
     
-    def test_set_percentage(self):
-        input_df = self.create_input_df()
-        expected_output_df = self.create_exp_output_df()
+    def test_split_sites_df(self, create_exp_percent_test_output_df):
+        """Test for the split_sites_df function."""
+        input_df = create_exp_percent_test_output_df
+        exp_to_apportion_df = self.create_exp_to_apportion_output()
+        exp_df_out = self.create_exp_remaining_output()
 
-        result_df = set_percentages(input_df)
-        assert_frame_equal(result_df, expected_output_df)
-   
+        imp_markers_to_keep: list = ["R", "TMI", "CF", "MoR", "constructed"]
+        result_to_apportion, result_df_out = split_sites_df(
+            input_df, 
+            imp_markers_to_keep
+        )
+
+        assert_frame_equal(result_to_apportion.reset_index(drop=True), exp_to_apportion_df)
+
+        assert_frame_equal(result_df_out.reset_index(drop=True), exp_df_out)
+
 
 class TestCreateNotnullMask():
     """Tests for the function create_not_null_mask."""
@@ -90,9 +171,9 @@ class TestCreateNotnullMask():
             
         data = [
             [0, 1],
-            [1, "a"],
-            [2, ""],
-            [3, pd.NA],
+            [111, "a"],
+            [222, ""],
+            [333, pd.NA],
             [4, np.nan],
             [5, " "],
         ]   
@@ -127,9 +208,9 @@ class TestCreateNotnullMask():
             
         data = [
             [0, 1, False, np.nan, np.nan, True, True, np.nan, np.nan],
-            [1, "a", True, np.nan, np.nan, True, True, 1.0, True],
-            [2, "", False, np.nan, True, True, True, 0.0, np.nan],
-            [3, pd.NA, False, True, np.nan, np.nan, np.nan, np.nan, np.nan],
+            [111, "a", True, np.nan, np.nan, True, True, 1.0, True],
+            [222, "", False, np.nan, True, True, True, 0.0, np.nan],
+            [333, pd.NA, False, True, np.nan, np.nan, np.nan, np.nan, np.nan],
             [4, np.nan, False, True, np.nan, np.nan, np.nan, np.nan, np.nan],
             [5, " ", True, np.nan, np.nan, True, True, 1.0, True],    
         ]   
@@ -141,7 +222,7 @@ class TestCreateNotnullMask():
         """Demonstrate how this function works and compare with other tests for null.
         
         The column under test has the following values:
-        [1, "a", "", pd.NA, np.nan, " "]
+        [111, "a", "", pd.NA, np.nan, " "]
         """
         demo_df = self.create_input_df()
 
@@ -171,7 +252,7 @@ class TestCreateNotnullMask():
         """Dataframe visualisation of tests for nulls.
         
         The column under test has the following values:
-        [1, "a", "", pd.NA, np.nan, " "]
+        [111, "a", "", pd.NA, np.nan, " "]
         """
         # set up an input dataframe 
         demo_df = self.create_input_df()
@@ -198,9 +279,9 @@ class TestCreateNotnullMask():
             
         data = [
             [0, 1,      np.nan, np.nan, True,   True,   np.nan, np.nan],  #noqa
-            [1, "a",    np.nan, np.nan, True,   True,   1.0,    True],  #noqa
-            [2, "",     np.nan, True,   True,   True,   0.0,    np.nan],  #noqa
-            [3, pd.NA,  True,   np.nan, np.nan, np.nan, np.nan, np.nan],  #noqa
+            [111, "a",    np.nan, np.nan, True,   True,   1.0,    True],  #noqa
+            [222, "",     np.nan, True,   True,   True,   0.0,    np.nan],  #noqa
+            [333, pd.NA,  True,   np.nan, np.nan, np.nan, np.nan, np.nan],  #noqa
             [4, np.nan, True,   np.nan, np.nan, np.nan, np.nan, np.nan],  #noqa
             [5, " ",    np.nan, np.nan, True,   True,   1.0,    True],  #noqa
         ]   
