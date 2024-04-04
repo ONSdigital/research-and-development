82a663ea1fd711d13d386ce4c41a1d2b5df28ca1
Browning <Charlie.Browning@ons.gov.uk>
2024-04-04 09:00:18 +0100

    feat: tests for create_period_year and region

--- tests/test_outputs/test_outputs_helpers.py
+++ tests/test_outputs/test_outputs_helpers.py
@@ -16,13 +16,47 @@ from src.outputs.outputs_helpers import (
 class TestCreateOutputDF(object):
     """Tests for create_output_df."""
 
+
 class TestRegions(object):
     """Tests for regions."""
+    
+    def test_regions(self):
+        """General Tests for Regions"""
+        output = regions()
+        assert isinstance(output, dict), (
+            "Output from 'regions' not of type 'dict'."
+        )
+        exp_keys = ["England", "Wales", "Scotland", "NI", "GB", "UK"]
+        assert sorted(exp_keys) == sorted(output.keys()), (
+            f"Dictionary returned by regions() expected keys {exp_keys}. Got "
+            f"{list(output.keys())}."
+        )
+
 
 class TestAggregateOutput(object):
     """Tests for aggregate_output."""
 
+
 class TestCreatePeriodYear(object):
     """Tests for create_period_year."""
+
+    @pytest.fixture(scope="function")
+    def period_year_input(self) -> pd.DataFrame:
+        """Test data for create_period_year."""
+        df_frame = {
+            "period": [202101, 202202, 202301, 202102, 202106]
+        }
+        df = pd.DataFrame(df_frame)
+        return df
+    
+    def test_create_period_year(self, period_year_input):
+        """General tests for create_period_year."""
+        output = create_period_year(period_year_input)
+        # assert correct periods have been determined
+        expected_periods = [2021, 2022, 2023, 2021, 2021]
+        resultant_periods = output.period_year.values
+        assert sorted(list(resultant_periods)) == sorted(expected_periods), (
+            "period_year column not as expected."
+        )
 
     
