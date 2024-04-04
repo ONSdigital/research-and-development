e04ce8b84833d16fa7bbc4ab632b01a619652cb2
Browning <Charlie.Browning@ons.gov.uk>
2024-04-04 08:43:11 +0100

    feat: test skelenton for output_helpers

--- tests/test_outputs/test_outputs_helpers.py
+++ tests/test_outputs/test_outputs_helpers.py
@@ -1,68 +1,28 @@
-from src.outputs.outputs_helpers import create_output_df
-from src.outputs.outputs_helpers import create_period_year
+"""Tests for outputs_helpers.py."""
+# Standard Library Imports
+import pytest
 
+# Third Party Imports
 import pandas as pd
-import unittest
-from pandas._testing import assert_frame_equal
 
+# Local Imports
+from src.outputs.outputs_helpers import (
+    create_output_df,
+    regions,
+    aggregate_output,
+    create_period_year
+)
 
-class TestCreateNewCols(unittest.TestCase):
-    """Unittest for create news columns"""
+class TestCreateOutputDF(object):
+    """Tests for create_output_df."""
 
-    def data_frame(self):
-        # create sample data
-        data = {"period": ["202201", "202206", "202109", "202112"]}
+class TestRegions(object):
+    """Tests for regions."""
 
-        df = pd.DataFrame(data)
-        return df
+class TestAggregateOutput(object):
+    """Tests for aggregate_output."""
 
-    def test_create_new_cols(self):
-        # Call the create_new_cols funtion
-        df_input = self.data_frame()
-        actual_result = create_period_year(df_input)
+class TestCreatePeriodYear(object):
+    """Tests for create_period_year."""
 
-        expected_result = pd.DataFrame(
-            {
-                "period": ["202201", "202206", "202109", "202112"],
-                "period_year": [2022, 2022, 2021, 2021],
-            }
-        )
-
-        assert_frame_equal(actual_result, expected_result)
-
-
-class TestSelectCols(unittest.TestCase):
-    """Unittest for selecting common columns from schema and df"""
-
-    def data_frame(self):
-        # create sample data
-        data = {"reference": [1234], "period": [202201], "unused": [0]}
-
-        df = pd.DataFrame(data)
-        return df
-
-    def schema(self):
-        # create sample schema
-        schema = {
-            "ref": {"old_name": "reference", "Deduced_Data_Type": "int64"},
-            "period": {"old_name": "period", "Deduced_Data_Type": "int64"},
-        }
-        return schema
-
-    def test_select_cols(self):
-        # Call the create_output_df funtion
-        df_input = self.data_frame()
-        schema = self.schema()
-        actual_result = create_output_df(df_input, schema)
-        expected_result = pd.DataFrame(
-            {
-                "ref": [1234],
-                "period": [202201],
-            }
-        )
-
-        assert_frame_equal(actual_result, expected_result)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    
