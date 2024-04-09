d682ab086d8d898296c54a7e299cb69dec140df4
Browning <Charlie.Browning@ons.gov.uk>
2024-04-09 11:22:30 +0100

    feat: tests for create_cora_status_col

--- tests/test_outputs/test_map_output_cols.py
+++ tests/test_outputs/test_map_output_cols.py
@@ -176,8 +176,76 @@ class TestMapSizebands(object):
     def test_map_sizebands(self, input_data, expected_output):
         """General tests for map_sizebands."""
         output = map_sizebands(input_data)
-        print(expected_output)
-        print(output)
         assert output.equals(expected_output), (
             "map_sizebands not working as expected."
         )
+
+
+class TestCreateCoraStatusCol(object):
+    """Tests for create_cora_status_col."""
+
+    @pytest.fixture(scope="function")
+    def input_data(self):
+        """Input data for create_cora_status_col tests."""
+        columns = ["col1", "form_status", "statusencoded"]
+        data = [
+            [1, np.nan, "100"],
+            [2, "200", np.nan], # default, no encoded
+            [3, np.nan, np.nan], # both nan
+            [4, "600", "302"], # both, but encoded points to different
+            [5, np.nan, "102"],
+        ]
+        df = pd.DataFrame(data=data, columns=columns)
+        return df
+    
+
+    @pytest.fixture(scope="function")
+    def exp_out(self):
+        """Expected output for create_cora_status_col tests."""
+        columns = ["col1", "form_status", "statusencoded"]
+        data = [
+            [1, "200", "100"],
+            [2, "200", np.nan], # default, no encoded
+            [3, np.nan, np.nan], # both nan
+            [4, "600", "302"], # both, but encoded points to different
+            [5, "1000", "102"],
+        ]
+        df = pd.DataFrame(data=data, columns=columns)
+        return df
+    
+
+    @pytest.fixture(scope="function")
+    def exp_out_no_fs(self):
+        """Expected output for create_cora_status_col tests."""
+        columns = ["col1", "stat_enc", "form_status"]
+        data = [
+            [1, "100", "200"],
+            [2, np.nan, np.nan], # default, no encoded
+            [3, np.nan, np.nan], # both nan
+            [4, "302", "1200"], # both, but encoded points to different
+            [5, "102", "1000"],
+        ]
+        df = pd.DataFrame(data=data, columns=columns)
+        return df
+    
+
+    def test_create_cora_status_col_fs(self, input_data, exp_out, exp_out_no_fs):
+        """General tests for create_core_status_col with form_status."""
+        # Test with form_status already created
+        output = create_cora_status_col(input_data)
+        assert output.equals(exp_out), (
+            "create_cora_status_col not acting as expected."
+        )
+        # Test that the function can create form_status and given a different
+        # main col
+        no_fs = input_data.copy().drop("form_status", axis=1)
+        no_fs = no_fs.rename(columns={"statusencoded": "stat_enc"})
+        no_fs_out = create_cora_status_col(no_fs, main_col="stat_enc")
+        assert ("form_status" in no_fs_out), (
+            "form_status column not created"
+        )
+        assert no_fs_out.equals(exp_out_no_fs), (
+            "create_cora_status_col not working with no form_status col and "
+            "specified main_col."
+        )
+        
