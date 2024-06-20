296b25b2fdf761d002c0fb69f7bbd33775510473
Rood <Matt.Rood@ons.gov.uk>
2024-06-20 16:39:11 +0100

    removed additional file change

--- cov_reports/update_readme.py
+++ cov_reports/update_readme.py
@@ -1,6 +1,5 @@
-import os
+import subprocess
 
-os.system("cd ..")
-os.system("pip3 install coverage readme-coverage-badger")
-os.system("python -m coverage run -m pytest")
-os.system("readme-cov")
+
+subprocess.run(["python", "-m", "coverage", "run", "-m", "pytest"])
+subprocess.run(["python", "-m", "readme-cov"])
