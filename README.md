e1666327ad735c6a76afd79a1a917346b41514b3
Browning <Charlie.Browning@ons.gov.uk>
2024-07-01 14:05:24 +0100

    fix: add omits to the readme updater

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Research and Development Project
 
-![Code Coverage](https://img.shields.io/badge/Coverage-37%25-red.svg)
+![Code Coverage](https://img.shields.io/badge/Coverage-46%25-orange.svg)
 
 Calculating national and regional research and development expenditure as part of [national accounts](https://www.ons.gov.uk/economy/nationalaccounts).
 
--- cov_reports/update_readme.py
+++ cov_reports/update_readme.py
@@ -2,5 +2,6 @@ import os
 
 os.system("cd ..")
 os.system("pip3 install coverage readme-coverage-badger")
-os.system("python -m coverage run -m --source=src pytest tests")
+OMIT_STR = "--omit=src/utils/hdfs_mods.py,src/utils/wrappers.py,src/utils/runlog.py,src/_version.py,src/pipeline.py,src/*_main.py"
+os.system(f"python -m coverage run -m --source=src {OMIT_STR} pytest tests")
 os.system("readme-cov")
