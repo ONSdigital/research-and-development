2e77309894612ecfe15d1a40e22bf8702c3cf79d
Browning <Charlie.Browning@ons.gov.uk>
2024-07-01 11:37:06 +0100

    merge with dev

--- cov_reports/update_readme.py
+++ cov_reports/update_readme.py
@@ -2,5 +2,5 @@ import os
 
 os.system("cd ..")
 os.system("pip3 install coverage readme-coverage-badger")
-os.system("python -m coverage run -m pytest")
+os.system("python -m coverage run -m --source=src pytest tests")
 os.system("readme-cov")
