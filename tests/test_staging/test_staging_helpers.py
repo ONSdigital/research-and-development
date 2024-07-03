421065cd4a43ff780ccd59cdd4822a45bc66b356
Browning <Charlie.Browning@ons.gov.uk>
2024-07-03 10:11:06 +0100

    fix: alter tests that still contained old config values

--- tests/test_staging/test_staging_helpers.py
+++ tests/test_staging/test_staging_helpers.py
@@ -275,7 +275,7 @@ class TestStageValidateHarmonisePostcodes(object):
     @pytest.fixture(scope="function")
     def config(self) -> pd.DataFrame:
         """Test config."""
-        config = {"global": {"postcode_csv_check": True}, "years": {"current_year": 2022}}
+        config = {"global": {"postcode_csv_check": True}, "years": {"survey_year": 2022}}
         return config
 
     def create_paths(self, pc_path, pc_ml) -> pd.DataFrame:
