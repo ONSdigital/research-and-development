843415e59dece7551dc40d355054b2c089892407
Rood <Matt.Rood@ons.gov.uk>
2024-07-10 09:58:25 +0100

    begun work to create schema

--- helpers/create_schema.py
+++ helpers/create_schema.py
@@ -8,9 +8,9 @@ import pandas as pd
 import os
 
 # Imputation file location and name
-input_dir = "D:\\work\\data\\res_dev"
+input_dir = "R:\\BERD Results System Development 2023\\DAP_emulation\\2022_surveys\\BERD\\05_mapping\\mapping_qa"
 
-pref = "Frozen_Group_Data2021_244 (1)"
+pref = "2022_full_responses_qa_24-07-10_v29"
 suff = ".csv"
 
 # Output folder for all schemas
