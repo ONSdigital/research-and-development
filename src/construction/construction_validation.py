79282883c9c6ef391b3f65af98f425ac69a50cb9
Browning <Charlie.Browning@ons.gov.uk>
2024-07-11 15:37:56 +0100

    feat: validation for duplicates in construction files; feat: associated tests

--- src/construction/construction_validation.py
+++ src/construction/construction_validation.py
@@ -1,3 +1,38 @@
 """Utilities to validate the construction process."""
+import logging
+from typing import Union
 
-# WILL BE POPULATED SOON WITH FUTURE CONSTRUCTION ADDITIONS/REFACTORING
+import pandas as pd
+
+from src.utils.defence import type_defence
+
+def check_for_duplicates(
+        df: pd.DataFrame, 
+        columns: Union[list, str], 
+        logger: logging.Logger = None
+    ) -> None:
+    """Check a dataframe for duplicate values across multiple columns.
+
+    Args:
+        df (pd.DataFrame): The dataframe to check for duplicates
+        columns (Union[list, str]): The columns to check duplicates across.
+        logger (logging.Logger, optional): The logger to log to. Defaults to None.
+    """
+    # defence checks
+    type_defence(df, "df", (pd.DataFrame))
+    type_defence(columns, "columns", (str, list))
+    type_defence(logger, "logger", (logging.Logger, type(None)))
+    if isinstance(columns, str):
+        columns = [columns]
+    for column in columns:
+        if column not in df.columns:
+            raise IndexError(f"Column {column} is not in the passed dataframe.")
+    # Check for duplicates
+    if logger:
+        logger.info("Checking construction dataframe for duplicates...")
+    refined_df = df[columns]
+    if not refined_df[refined_df.duplicated()].empty:
+        raise ValueError("Duplicates found in construction file. Aborting pipeline.")
+    if logger:
+        logger.info("No duplicates found in construction files. Continuing...")
+    return None
