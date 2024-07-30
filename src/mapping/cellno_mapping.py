2e21134c139d426375a922b1f2c6147078757844
Anne Griffith <anne.griffith@ons.gov.uk>
2024-07-30 12:06:00 +0100

    946 correction for removing cellno count check

--- src/mapping/cellno_mapping.py
+++ src/mapping/cellno_mapping.py
@@ -9,18 +9,16 @@ from src.mapping.mapping_helpers import check_mapping_unique, join_with_null_che
 MappingLogger = logging.getLogger(__name__)
 
 
-def clean_validate_cellno_mapper(cellno_df: pd.DataFrame, num: int) -> pd.DataFrame:
+def clean_validate_cellno_mapper(cellno_df: pd.DataFrame) -> pd.DataFrame:
     """Clean and validate the cellno mapper dataframe.
 
     Args:
         cellno_df (pd.DataFrame): The cellnumber mapper dataframe.
-        num (int): The expected number of cellnumbers.
 
     Returns:
         pd.DataFrame: The cleaned and validated cellnumber mapper dataframe.
 
     Raises:
-        ValueError: If the number of cellnumbers is not as expected.
         ValueError: If the cellnumbers are not in the expected range.
     """
     check_mapping_unique(cellno_df, "cell_no")
@@ -49,8 +47,7 @@ def validate_join_cellno_mapper(
         pd.DataFrame: The shortform responses dataframe with a column for universe count
     """
     gb_df, ni_df = responses
-    num = config["estimation"]["num_expected_cellnos"]
-    cellno_df = clean_validate_cellno_mapper(cellno_df, num)
+    cellno_df = clean_validate_cellno_mapper(cellno_df)
     gb_df = join_with_null_check(gb_df, cellno_df, "cellno", "cellnumber")
 
     return gb_df, ni_df
