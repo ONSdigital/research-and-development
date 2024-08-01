09d14cec13a7aa93f93a6c242f149c8e0df3bba9
George Zorinyants <george.zorinyants@ons.gov.uk>
2024-08-01 12:52:41 +0000

    Load json file

--- cdp_test_db_gz.py
+++ cdp_test_db_gz.py
@@ -1,5 +1,6 @@
 import boto3
 import raz_client
+import json
 import pandas as pd
 from io import StringIO
 
@@ -18,13 +19,14 @@ raz_client.configure_ranger_raz(client, ssl_file=config["ssl_file"])
 list_files(client, config["s3_bucket"], "/user/george.zorinyants")
 
 ################################################################################
+# Read a CSV file into a Pandas dataframe
 with client.get_object(Bucket=config["s3_bucket"], Key='user/george.zorinyants/pg_num_alpha_2023.csv')['Body'] as csv_f:
   rescue_df = pd.read_csv(csv_f)
   
 rescue_df.head()
 
 ################################################################################
-# Create a dataframe and write a file
+# Create a dataframe and write to a CSV file
 my_data = {"coutry": ["a", "b"], "value": [1, 2]}
 df = pd.DataFrame(my_data)
 df.head()
@@ -34,8 +36,19 @@ csv_buffer = StringIO()
 df.to_csv(csv_buffer, header=True, index=False)
 csv_buffer.seek(0) # To position the stream at the start of the buffer
 
-client.put_object(
+_ = client.put_object(
     Bucket=config["s3_bucket"], 
     Body=csv_buffer.getvalue(), 
     Key='user/george.zorinyants/test_output.csv'
 )
+
+###############################################################################
+# Read a json file into a Python dictionary
+with client.get_object(
+    Bucket=config["s3_bucket"], 
+    Key='user/george.zorinyants/snapshot.json'
+)['Body'] as json_file:
+  
+    my_dict = json.load(json_file)
+    
+print(my_dict)
