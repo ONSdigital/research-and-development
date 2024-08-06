091e20b6a37b21d492dbe74e38939aceba96b5c0
Browning <Charlie.Browning@ons.gov.uk>
2024-08-06 14:01:45 +0100

    feat: a function to add 'last_frozen' column to frozen data

--- src/freezing/freezing_main.py
+++ src/freezing/freezing_main.py
@@ -5,6 +5,7 @@ from typing import Callable
 import pandas as pd
 
 from src.staging.validation import validate_data_with_schema
+from src.utils.defence import type_defence
 
 
 FreezingLogger = logging.getLogger(__name__)
@@ -96,3 +97,24 @@ def run_freezing(
 def read_frozen_csv():
     # new functionality
     pass
+
+# function ready for use
+def _add_last_frozen_column(
+        frozen_df: pd.DataFrame, 
+        run_id: int
+    ) -> pd.DataFrame:
+    """Add the last_frozen column to staged data.
+
+    Args:
+        frozen_df (pd.DataFrame): The frozen data.
+        run_id (int): The current run id.
+
+    Returns:
+        pd.DataFrame: A dataframe containing the updated last_frozen column.
+    """
+    type_defence(frozen_df, "frozen_df", pd.DataFrame)
+    type_defence(run_id, "run_id", int)
+    todays_date = datetime.today().strftime("%d_%m_%Y")
+    last_frozen = f"{todays_date}_{str(run_id)}"
+    frozen_df["last_frozen"] = last_frozen
+    return frozen_df
