6d6599d8bad75bd395e3bbece4a5246cd3f07366
Browning <Charlie.Browning@ons.gov.uk>
2024-08-13 13:44:39 +0100

    feat: tests for values_in_column

--- tests/test_utils/test_helpers.py
+++ tests/test_utils/test_helpers.py
@@ -1,4 +1,9 @@
-from src.utils.helpers import convert_formtype
+"""Tests for utils.helpers."""
+
+import pytest
+import pandas as pd
+
+from src.utils.helpers import convert_formtype, values_in_column
 
 def test_convert_formtype():
     """Test for convert_formtype()."""
@@ -13,3 +18,36 @@ def test_convert_formtype():
     assert convert_formtype("2") is None, msg
     assert convert_formtype("") is None, msg
     assert convert_formtype(None) is None, msg
+
+
+class TestValuesInColumn(object):
+    """Tests for values_in_column."""
+
+    @pytest.fixture(scope="function")
+    def dummy_df(self):
+        """A dummy dataframe for testing."""
+        df = pd.DataFrame(
+            {"col": [1, 2, 3, 5, 6, 8, 0]}
+        )
+        return df
+    
+
+    @pytest.mark.parametrize(
+            "values, expected",
+            [
+                ([1, 2, 3], True),#True/List
+                (pd.Series([0, 5, 8]), True),#True/Series
+                ([1, 2, 4], False),#False/List
+                (pd.Series([15]), False),#False/Series
+            ]
+    )
+    def test_valuies_in_column(self, dummy_df, values, expected):
+        """General tests for values_in_column."""
+        result = values_in_column(
+            df=dummy_df,
+            col_name="col",
+            values=values
+        )
+        assert result == expected, (
+            "Retruned value from values_in_column not as expected."
+        )
