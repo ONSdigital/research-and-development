23a696cda990ed0df4345f22e47802cea66db007
Cheshire <Jen.Cheshire@ons.gov.uk>
2024-08-14 08:52:04 +0100

    RDRP-918: added all cols of interest to check

--- src/freezing/freezing_compare.py
+++ src/freezing/freezing_compare.py
@@ -26,20 +26,23 @@ def get_amendments(
     FreezingLogger.info("Looking for records that have changed in the updated snapshot.")
     key_cols = ["reference", "period", "instance"]
     numeric_cols = [
-        "219",
-        "220",
-        "242",
-        "243",
-        "244",
-        "245",
-        "246",
-        "247",
-        "248",
-        "249",
-        "250",
+        "201", "202", "203", "204", "205", "206", "207", "209", "210",
+        "211", "212", "214", "216", "218", "219", "220", "221", "222",
+        "223", "225", "226", "227", "228", "229", "237", "242", "243",
+        "244", "245", "246", "247", "248", "249", "250", "405", "406",
+        "407", "408", "409", "410", "411", "412", "501", "502", "503",
+        "504", "505", "506", "507", "508", "602", "701", "702", "703",
+        "704", "705", "706", "707", "709", "711",
+    ]
+
+    non_numeric_cols = [
+        "200",
+        "601",
     ]
     numeric_cols_new = [f"{i}_updated" for i in numeric_cols]
     numeric_cols_diff = [f"{i}_diff" for i in numeric_cols]
+    non_numeric_cols_new = [f"{i}_updated" for i in non_numeric_cols]
+    non_numeric_cols_diff = [f"{i}_diff" for i in non_numeric_cols]
 
     # Inner join on keys to select only records present in both snapshots
     amendments_df = pd.merge(
@@ -58,18 +61,26 @@ def get_amendments(
                 amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"]
             )
             amendments_df.loc[
-                amendments_df[f"{each}_diff"] > 0.00001, f"is_{each}_diff_nonzero"
+                amendments_df[f"{each}_diff"] > 0.00001, f"is_{each}_diff_nonzero_or_true"
             ] = True
 
-        # Take a slice of the df which is just the cols ending with _diff_nonzero
+        for each in non_numeric_cols:
+            amendments_df[f"is_{each}_diff_nonzero_or_true"] = (
+                amendments_df[f"{each}_updated"] != amendments_df[f"{each}_original"]
+            )
+            amendments_df.loc[
+                amendments_df[f"is_{each}_diff_nonzero_or_true"], f"{each}_diff"
+            ] = amendments_df[f"{each}_updated"]
+
+        # Take a slice of the df which is just the cols ending with _diff_nonzero_or_true
         # Do a column-wise any() on this slice, which returns a series where the
-        # value is True if any of the *_diff_nonzero cols in that row were True
+        # value is True if any of the *_diff_nonzero_or_true cols in that row were True
         # Add that series as a column to the original df
-        # Remove any rows from the df where is_any_diff_nonzero is False
-        amendments_df["is_any_diff_nonzero"] = amendments_df[
-            amendments_df.columns[amendments_df.columns.str.endswith("_diff_nonzero")]
+        # Remove any rows from the df where is_any_diff_nonzero_or_true is False
+        amendments_df["is_any_diff_nonzero_or_true"] = amendments_df[
+            amendments_df.columns[amendments_df.columns.str.endswith("_diff_nonzero_or_true")]
         ].any(axis="columns")
-        amendments_df = amendments_df.loc[amendments_df["is_any_diff_nonzero"]]
+        amendments_df = amendments_df.loc[amendments_df["is_any_diff_nonzero_or_true"]]
 
         # Select the row from the updated snapshot, and differences in key variables
         select_cols = [
@@ -78,6 +89,7 @@ def get_amendments(
             "instance",
             *[col for col in amendments_df.columns if col.endswith("_updated")],
             *numeric_cols_diff,
+            *non_numeric_cols_diff,
         ]
         amendments_df = amendments_df[select_cols]
         amendments_df.columns = [col.replace("_updated", "") for col in amendments_df.columns]
