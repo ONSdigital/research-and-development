7cb241ab0aa224185c6f41d27578e8e90b40bf0b
zoring <George.Zorinyants@ons.gov.uk>
2024-09-13 09:49:44 +0100

    Removed full paths from the code, left relative paths only

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ start_dir = os.getcwd()
 if start_dir.startswith("/home"):
     os.chdir("research-and-development")
 print(f"Current directory is {os.getcwd()}")
-# import src.pipeline as src
+import src.pipeline as src
 
-# # reload the pipeline module to implement any changes
-# reload(src)
+# reload the pipeline module to implement any changes
+reload(src)
 
-# user_path = os.path.join("/home/cdsw/research-and-development/src/", "user_config.yaml")
-# dev_path = os.path.join("/home/cdsw/research-and-development/src/", "dev_config.yaml")
-# os.chdir("/home/cdsw/research-and-development/")
+user_path = os.path.join("src", "user_config.yaml")
+dev_path = os.path.join("src", "dev_config.yaml")
 
-# start = time.time()
-# run_time = src.run_pipeline(user_path, dev_path)
+start = time.time()
+run_time = src.run_pipeline(user_path, dev_path)
 
-# min_secs = divmod(round(run_time), 60)
+min_secs = divmod(round(run_time), 60)
 
-# print(f"Time taken for pipeline: {min_secs[0]} mins and {min_secs[1]} seconds")
+print(f"Time taken for pipeline: {min_secs[0]} mins and {min_secs[1]} seconds")
